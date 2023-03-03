55c48910b8e624014a31ea57b4cbb436541ab62e
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-03-03 10:31:53 +0900

    [unrated] Title: 덧칠하기, Time: 26.43 ms, Memory: 66.9 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.kt
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.kt
@@ -2,21 +2,17 @@ import java.util.*
 
 class Solution {
     fun solution(n: Int, m: Int, section: IntArray): Int {
-        var answer = 1
+        var answer = 0
         val queue = LinkedList<Int>()
         queue.addAll(section.toList())
 
         while (queue.isNotEmpty()) {
             val num = queue.poll()!!
+            answer++
 
             while (queue.isNotEmpty()) {
                 val next = queue.peek()!!
-                if (next > num + m - 1) {
-                    answer++
-                    break
-                } else {
-                    queue.poll()
-                }
+                if (next > num + m - 1) break else queue.poll()
             }
         }
         return answer
