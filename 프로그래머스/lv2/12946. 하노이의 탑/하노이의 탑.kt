b6b2455fc5ddb6b0c2b3c6d47da6315eddc9da22
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-03-11 23:22:12 +0900

    [level 2] Title: 하노이의 탑, Time: 2.24 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.kt
+++ 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.kt
@@ -1,19 +1,16 @@
 class Solution {
     var answer = mutableListOf<IntArray>()
+    
     fun solution(n: Int): Array<IntArray> {
         moveTower(n, 1, 3)
         return answer.toTypedArray()
     }
 
     fun moveTower(size: Int, start: Int, destination: Int) {
-        if (size > 2) {
+        if (size > 0) {
             moveTower(size - 1, start, 6 - start - destination)
             answer.add(intArrayOf(start, destination))
             moveTower(size - 1, 6 - start - destination, destination)
-        } else if (size == 2) {
-            answer.add(intArrayOf(start, 6 - start - destination))
-            answer.add(intArrayOf(start, destination))
-            answer.add(intArrayOf(6 - start - destination, destination))
         }
     }
 }
