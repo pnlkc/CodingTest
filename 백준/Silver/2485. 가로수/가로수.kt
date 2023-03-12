a0d09c4bce6d67b592b50108e848fd7edaff5e60
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-03-12 14:12:42 +0900

    [Silver IV] Title: 가로수, Time: 304 ms, Memory: 26480 KB -BaekjoonHub

--- 백준/Silver/2485. 가로수/가로수.kt
+++ 백준/Silver/2485. 가로수/가로수.kt
@@ -1,20 +1,18 @@
 fun main() {
     val tcNum = readln().toInt()
-    val list = mutableListOf<Int>()
-    repeat(tcNum) {
-        list.add(readln().toInt())
+    val gapList = mutableListOf<Long>()
+    var previousNum = readln().toLong()
+
+    repeat(tcNum - 1) {
+        val input = readln().toLong()
+        gapList.add(input - previousNum)
+        previousNum = input
     }
 
-    val gapList = mutableListOf<Int>()
-
-    for (i in 0 until list.lastIndex) {
-        gapList.add(list[i + 1] - list[i])
-    }
-    
     var gcd = gapList[0]
     gapList.forEach { gcd = gcd(gcd, it) }
 
-    println(gapList.sumOf { it / gcd } - gapList.size)
+    println(gapList.sum() / gcd - gapList.size)
 }
 
-fun gcd(a: Int, b: Int): Int = if (b != 0) gcd(b, a % b) else a
+fun gcd(a: Long, b: Long): Long = if (b != 0L) gcd(b, a % b) else a
