aa0ae487330afd2c188b2b87fe77252e275e8c52
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-03-13 12:04:47 +0900

    [Silver II] Title: 참외밭, Time: 168 ms, Memory: 23804 KB -BaekjoonHub

--- 백준/Silver/2477. 참외밭/참외밭.kt
+++ 백준/Silver/2477. 참외밭/참외밭.kt
@@ -5,6 +5,7 @@ fun main() {
         list.add(readln().split(" ").map { it.toInt() })
     }
 
+    // 입력값들을 좌표로 변경
     val posList = mutableListOf<List<Int>>()
     val pos = intArrayOf(0, 0)
     list.forEach {
@@ -17,12 +18,18 @@ fun main() {
         posList.add(pos.toList())
     }
 
+    // 큰 사각형 값 구하기
     val min = listOf(posList.minOf { it[0] }, posList.minOf { it[1] })
     val max = listOf(posList.maxOf { it[0] }, posList.maxOf { it[1] })
+    val bigSquare = (max[0] - min[0]) * (max[1] - min[1])
+
+    // 작은 사각형 값 구하기
     val center = posList.find { it[0] != min[0] && it[0] != max[0] && it[1] != min[1] && it[1] != max[1] }!!
     val noExist = listOf(min, max, listOf(min[0], max[1]), listOf(max[0], min[1]))
         .filter { vl -> posList.count { it[0] == vl[0] && it[1] == vl[1]} == 0 }[0]
-    val bigSquare = (max[0] - min[0]) * (max[1] - min[1])
     val smallSquare = (center[0] - noExist[0]) * (center[1] - noExist[1])
+
+    // 큰 사각형 - 작은 사각형 = 참외 밭의 넓이
+    // 1m^2에서 자라는 참외 수 * 참외 밭의 넓이 = 밭에서 자라는 참외의 수
     println(num * (bigSquare - if (smallSquare < 0) smallSquare * -1 else smallSquare))
 }
