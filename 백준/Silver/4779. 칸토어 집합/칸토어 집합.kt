4034786b6037970946e9313adc6e95b0876c3766
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-03-22 10:52:56 +0900

    [Silver III] Title: 칸토어 집합, Time: 392 ms, Memory: 31904 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.kt
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.kt
@@ -3,7 +3,7 @@ import kotlin.math.pow
 
 fun main() = with(Scanner(System.`in`)) {
     while (hasNextInt()) {
-        val num = 3.0.pow(nextDouble()).toInt()
+        val num = 3.0.pow(nextInt()).toInt()
         val array = CharArray(num) { '-' }
         calc(array, 0, num - 1)
         println(array.joinToString(""))
@@ -14,7 +14,7 @@ fun calc(array: CharArray, s: Int, e: Int) {
     if (s == e) return
     val r = (e - s + 1) / 3
     calc(array, s, s + r - 1)
-    for (i in s + r until  s+ r + r) {
+    for (i in s + r..e - r) {
         array[i] = ' '
     }
     calc(array, s + r + r, e)
