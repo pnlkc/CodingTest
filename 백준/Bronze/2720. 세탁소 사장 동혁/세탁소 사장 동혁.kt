293253dec1cf62e7266dc70f83fde165eac40c51
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-03-24 12:02:34 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 156 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.kt
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.kt
@@ -1,10 +1,10 @@
 fun main() {
     repeat(readln().toInt()) {
-        val answer = mutableListOf<Int>()
+        var answer = ""
         var num = readln().toInt()
 
         val calc: (Int) -> Unit = { n ->
-            answer.add(num / n)
+            answer += if (n != 25) " " + num / n else num / n
             num %= n
         }
 
@@ -13,6 +13,6 @@ fun main() {
         calc(5)
         calc(1)
 
-        println(answer.joinToString(" "))
+        println(answer)
     }
 }
