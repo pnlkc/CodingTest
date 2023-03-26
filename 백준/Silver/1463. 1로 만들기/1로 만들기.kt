bec12a381aaf90c0d7f77ceef5b04619ff8c29bb
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-03-26 11:13:54 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 16628 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.kt
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.kt
@@ -2,21 +2,10 @@ fun main() {
     val num = readln().toInt()
     val memo = IntArray(num + 1) { Int.MAX_VALUE }
     memo[num] = 0
-    calc(memo, num)
-    println(memo[1])
-}
-
-fun calc(memo: IntArray, num: Int) {
-    if (num <= 1) return
-
-    val logic: (Int) -> Unit = { n ->
-        if (memo[n] > memo[num] + 1) {
-            memo[n] = memo[num] + 1
-            calc(memo, n)
-        }
+    for (i in num downTo 2) {
+        memo[i - 1] = minOf(memo[i] + 1, memo[i - 1])
+        if (i % 3 == 0) memo[i / 3] = minOf(memo[i] + 1, memo[i / 3])
+        if (i % 2 == 0) memo[i / 2] = minOf(memo[i] + 1, memo[i / 2])
     }
-
-    if (num % 3 == 0) logic(num / 3)
-    if (num % 2 == 0) logic(num / 2)
-    logic(num - 1)
+    println(memo[1])
 }
