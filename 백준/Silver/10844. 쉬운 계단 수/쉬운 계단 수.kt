803b54ee33516ba2606acd08ffbde1527b3f7305
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-03-26 12:04:45 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 92 ms, Memory: 12304 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.kt
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.kt
@@ -1,21 +1,15 @@
 fun main() {
     val num = readln().toInt()
-    val array = Array(num) {
-        mutableMapOf(0 to 0, 1 to 1, 2 to 1, 3 to 1, 4 to 1, 5 to 1, 6 to 1, 7 to 1, 8 to 1, 9 to 1)
-    }
+    val arr = Array(num) { IntArray(10) { 1 } }
+    arr[0][0] = 0
 
     for (i in 1 until num) {
-        array[i][0] = array[i - 1][1]!!
-        array[i][1] = (array[i - 1][0]!! + array[i - 1][2]!!) % 1_000_000_000
-        array[i][2] = (array[i - 1][1]!! + array[i - 1][3]!!) % 1_000_000_000
-        array[i][3] = (array[i - 1][2]!! + array[i - 1][4]!!) % 1_000_000_000
-        array[i][4] = (array[i - 1][3]!! + array[i - 1][5]!!) % 1_000_000_000
-        array[i][5] = (array[i - 1][4]!! + array[i - 1][6]!!) % 1_000_000_000
-        array[i][6] = (array[i - 1][5]!! + array[i - 1][7]!!) % 1_000_000_000
-        array[i][7] = (array[i - 1][6]!! + array[i - 1][8]!!) % 1_000_000_000
-        array[i][8] = (array[i - 1][7]!! + array[i - 1][9]!!) % 1_000_000_000
-        array[i][9] = (array[i - 1][8]!!)
+        for (j in 0..9) {
+            val f = if (j - 1 in 0..8) arr[i - 1][j - 1] else 0
+            val e = if (j + 1 in 1..9) arr[i - 1][j + 1] else 0
+            arr[i][j] = (f + e) % 1_000_000_000
+        }
     }
 
-    println(array[num - 1].values.sumOf { it.toLong() } % 1_000_000_000)
+    println(arr[num - 1].sumOf { it.toLong() } % 1_000_000_000)
 }
