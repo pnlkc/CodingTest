2bf0fe42e1e234af8c035d93045621e612e5b57e
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-03-27 11:17:11 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 276 ms, Memory: 27416 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.kt
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.kt
@@ -3,8 +3,7 @@ fun main() {
     val arr = readln().split(" ").map { intArrayOf(it.toInt(), 1) }
 
     for (i in 1 until num) {
-        val filter = arr.slice(0..i).filter { it[0] < arr[i][0] }
-        if (filter.isNotEmpty()) arr[i][1] = filter.maxOf { it[1] } + 1
+        arr[i][1] += arr.filterIndexed { idx, ints -> idx < i && ints[0] < arr[i][0] }.maxOfOrNull { it[1] } ?: 0
     }
 
     println(arr.maxOf { it[1] })
