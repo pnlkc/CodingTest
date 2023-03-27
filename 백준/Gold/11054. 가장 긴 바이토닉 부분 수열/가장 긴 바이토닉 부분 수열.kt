699d79ebed767f8b4dd01fac093f97af9662c70a
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-03-27 11:51:03 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 492 ms, Memory: 33200 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.kt
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.kt
@@ -1,23 +1,20 @@
 fun main() {
     val num = readln().toInt()
-    val arr = readln().split(" ").map { intArrayOf(it.toInt(), 1, 1, 1) }
+    val arr = readln().split(" ").map { intArrayOf(it.toInt(), 1, 1) }
 
-    for (i in 1 until num) {
-        // 계속 증가
+    // 증가
+    for (i in 0 until num) {
         arr[i][1] += arr
             .filterIndexed { idx, ints -> idx < i && ints[0] < arr[i][0] }
             .maxOfOrNull { it[1] } ?: 0
-
-        // 꺾임
-        arr[i][2] += arr
-            .filterIndexed { idx, ints -> idx < i && ints[0] < arr[i][0] }
-            .maxOfOrNull { it[1] } ?: 0
-
-        // 계속 감소
-        arr[i][3] += arr
-            .filterIndexed { idx, ints -> idx < i && ints[0] > arr[i][0] }
-            .maxOfOrNull { maxOf(it[2], it[3]) } ?: 0
     }
 
-    println(arr.maxOf { maxOf(it[1], it[2], it[3]) })
+    // 감소
+    for (i in num - 1 downTo 0) {
+        arr[i][2] += arr
+            .filterIndexed { idx, ints -> idx > i && ints[0] < arr[i][0] }
+            .maxOfOrNull { it[2] } ?: 0
+    }
+
+    println(arr.maxOf { it[1] + it[2] - 1 })
 }
