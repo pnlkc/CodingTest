1df82802535ac351db06e1cc5af7786a054a2a3e
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-03-28 11:26:37 +0900

    [Gold V] Title: 전깃줄, Time: 160 ms, Memory: 19536 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.kt
+++ 백준/Gold/2565. 전깃줄/전깃줄.kt
@@ -1,20 +1,18 @@
-/**
- * 전깃줄 문제의 핵심은 전깃줄이 서로 겹치지 않으려면 A, B 기둥의 번호가 모두 커야 한다는 것이다.
- * 동적 계획법 알고리즘의 LIS(Longest Increasing Subsequence)를 사용해 문제를 풀면 된다.
- */
-
 fun main() {
     val tcNum = readln().toInt()
-    val arr = Array(tcNum) { readln().split(" ").map { it.toInt() }.toIntArray() + intArrayOf(1) }
+    val arr = Array(tcNum) { readln().split(" ").map { it.toInt() } }
+    val memo = IntArray(tcNum) { 1 }
+    var max = 0
 
     // 왼쪽 기둥 번호를 기준으로 오름차순 정렬
     arr.sortBy { it[0] }
 
-    for (n in 1 until tcNum) {
-        arr[n][2] += arr
-            .filterIndexed { i, ints -> i <= n && ints[0] < arr[n][0] && ints[1] < arr[n][1] }
-            .maxOfOrNull { it[2] } ?: 0
+    for (i in 1 until tcNum) {
+        for (j in 0 until i) {
+            if (arr[i][1] > arr[j][1]) memo[i] = maxOf(memo[i], memo[j] + 1)
+        }
+        max = maxOf(max, memo[i])
     }
 
-    println(tcNum - arr.maxOf { it[2] })
+    println(tcNum - max)
 }
