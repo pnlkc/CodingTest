4a785fad62392866a6175447bfbd6e3b11f8d562
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-03-28 11:14:52 +0900

    Update 전깃줄.kt

--- 백준/Gold/2565. 전깃줄/전깃줄.kt
+++ 백준/Gold/2565. 전깃줄/전깃줄.kt
@@ -1,3 +1,8 @@
+/**
+ * 전깃줄 문제의 핵심은 전깃줄이 서로 겹치지 않으려면 A, B 기둥의 번호가 모두 커야 한다는 것이다.
+ * 동적 계획법 알고리즘의 LIS(Longest Increasing Subsequence)를 사용해 문제를 풀면 된다.
+ */
+
 fun main() {
     val tcNum = readln().toInt()
     val arr = Array(tcNum) { readln().split(" ").map { it.toInt() }.toIntArray() + intArrayOf(1) }
