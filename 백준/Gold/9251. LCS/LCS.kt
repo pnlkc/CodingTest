c5e1bf593c53392ad0217d4a20fd0fbd4e7c8367
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-03-28 12:04:26 +0900

    [Gold V] Title: LCS, Time: 144 ms, Memory: 18316 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val s1 = readln()
+    val s2 = readln()
+    val memo = Array(s1.length + 1) { IntArray(s2.length + 1) }
+    var max = 0
+
+    for (i in 1..s1.length) {
+        for (j in 1..s2.length) {
+            if (s1[i - 1] == s2[j - 1]) memo[i][j] = memo[i - 1][j - 1] + 1
+            else memo[i][j] = maxOf(memo[i - 1][j], memo[i][j - 1])
+            max = maxOf(max, memo[i][j])
+        }
+    }
+
+    println(max)
+}
