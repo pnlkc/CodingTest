62f1808cc3e6bd9b2d9984979e3ea0c9031401f9
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-03-28 11:10:44 +0900

    [Gold V] Title: 전깃줄, Time: 188 ms, Memory: 24276 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.kt
+++ 백준/Gold/2565. 전깃줄/전깃줄.kt
@@ -5,9 +5,10 @@ fun main() {
     // 왼쪽 기둥 번호를 기준으로 오름차순 정렬
     arr.sortBy { it[0] }
 
-    for (i in 1 until tcNum) {
-        val filter = arr.slice(0..i).filter { it[0] < arr[i][0] && it[1] < arr[i][1] }
-        if (filter.isNotEmpty()) arr[i][2] = filter.maxOf { it[2] } + 1
+    for (n in 1 until tcNum) {
+        arr[n][2] += arr
+            .filterIndexed { i, ints -> i <= n && ints[0] < arr[n][0] && ints[1] < arr[n][1] }
+            .maxOfOrNull { it[2] } ?: 0
     }
 
     println(tcNum - arr.maxOf { it[2] })
