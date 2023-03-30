f41efc61b024b346155361720635d6f4f9ffbfb2
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-03-30 13:35:38 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 236 ms, Memory: 25120 KB -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.kt
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.kt
@@ -1,18 +1,30 @@
+import java.io.*
+
 fun main() {
-    val s = readln()
-    val num = readln().toInt()
-    val map = mutableMapOf<String, IntArray>()
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val string = StringBuilder()
+
+    val s = br.readLine()
+    val num = br.readLine().toInt()
+    val arr = Array(s.length + 1) { mutableMapOf<Char, Int>() }
+    val exist = s.toSet()
+    exist.forEach { arr[0][it] = 0 }
+
+    s.forEachIndexed { idx, c ->
+        exist.forEach {
+            if (it == c) arr[idx + 1][it] = arr[idx][it]!! + 1
+            else arr[idx + 1][it] = arr[idx][it]!!
+        }
+    }
 
     repeat(num) {
-        val (c, l, r) = readln().split(" ")
-
-        if (map[c] == null) {
-            map[c] = IntArray(s.length + 1)
-            for (i in 1..s.length) {
-                map[c]!![i] = map[c]!![i - 1] + if (s[i - 1].toString() == c) 1 else 0
-            }
-        }
-
-        println(map[c]!![r.toInt() + 1] - map[c]!![l.toInt()])
+        val (c, l, r) = br.readLine().split(" ")
+        if (!exist.contains(c[0])) string.append("${0}\n")
+        else string.append("${arr[r.toInt() + 1][c[0]]!! - arr[l.toInt()][c[0]]!!}\n")
     }
+
+    bw.write(string.toString())
+    bw.flush()
+    bw.close()
 }
