79934ee9b9cb775b17b33442a72308681cd7711e
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-03-30 10:52:57 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 92 ms, Memory: 12312 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.kt
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.kt
@@ -1,7 +1,7 @@
 fun main() {
-    calc((1).toBigInteger(), readln().toInt())
+    calc(1L, readln().toInt())
 }
 
-fun calc(answer: java.math.BigInteger, i: Int) {
-    if (i <= 1) println(answer) else calc(answer.multiply(i.toBigInteger()), i - 1)
+fun calc(answer: Long, i: Int) {
+    if (i <= 1) println(answer) else calc(answer * i, i - 1)
 }
