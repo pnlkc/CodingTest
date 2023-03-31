0fc007cfadfed8a60806ad619a492e820142cb5f
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-03-31 10:49:47 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 196 ms, Memory: 22892 KB -BaekjoonHub

--- 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.kt
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.kt
@@ -1,17 +1,14 @@
 fun main() {
     val map = mutableMapOf("ChongChong" to true)
-    var count = 1
 
     repeat(readln().toInt()) {
         val (p1, p2) = readln().split(" ")
         if (map[p1] == true && map[p2] != true) {
             map[p2] = true
-            count++
         } else if (map[p2] == true && map[p1] != true) {
             map[p1] = true
-            count++
         }
     }
-    
-    println(count)
+
+    println(map.size)
 }
