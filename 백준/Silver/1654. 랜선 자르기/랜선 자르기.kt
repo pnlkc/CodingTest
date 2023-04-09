6beaf8950aa02f7fa5dd590ce8b900a3dfb15b32
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-04-09 13:30:04 +0900

    [Silver II] Title: 랜선 자르기, Time: 292 ms, Memory: 26428 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.kt
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.kt
@@ -3,20 +3,10 @@ fun main() {
     val arr = IntArray(k) { readln().toInt() }.also { it.sort() }
     var s = 1L
     var e = arr.last().toLong()
-    
+
     p@while (s <= e) {
-        var count = 0L
         val mid = (s + e) / 2
-
-        for (i in 0 until k) {
-            count += arr[i] / mid
-            if (count >= n) {
-                s = mid + 1
-                continue@p
-            }
-        }
-
-        e = mid - 1
+        if (arr.sumOf { it / mid } >= n) s = mid + 1 else e = mid - 1
     }
 
     println(e)
