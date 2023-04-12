d0b66b1665ad309068114d65988360a458b15d6c
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-04-12 09:14:09 +0900

    [Silver II] Title: 최대 힙, Time: 292 ms, Memory: 25920 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.kt
+++ 백준/Silver/11279. 최대 힙/최대 힙.kt
@@ -1,15 +1,17 @@
+import java.io.*
 import java.util.*
 
 fun main() {
-    val tcNum = readln().toInt()
+    val bw = BufferedReader(InputStreamReader(System.`in`))
     val pQueue = PriorityQueue<Int>(reverseOrder())
-    
-    repeat(tcNum) {
-        when(val input = readln().toInt()) {
-            0 -> {
-                println(if (pQueue.isEmpty()) 0 else pQueue.poll())
-            }
+    val s = StringBuilder()
+
+    repeat(bw.readLine().toInt()) {
+        when(val input = bw.readLine().toInt()) {
+            0 -> s.appendLine(if (pQueue.isEmpty()) 0 else pQueue.poll())
             else -> pQueue.add(input)
         }
     }
+
+    println(s.toString())
 }
