bce58c39ba8c8aa2a93a12b4196bca72360ad6c6
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-04-18 12:44:57 +0900

    [Gold III] Title: 양팔저울, Time: 180 ms, Memory: 21116 KB -BaekjoonHub

--- 백준/Gold/2629. 양팔저울/양팔저울.kt
+++ 백준/Gold/2629. 양팔저울/양팔저울.kt
@@ -1,25 +1,24 @@
 fun main() {
     val n1 = readln().toInt()
-    val arr1 = readln().split(" ").map { it.toInt() }.sorted().toIntArray()
+    val arr1 = readln().split(" ").map { it.toInt() }
     val n2 = readln().toInt()
-    val arr2 = readln().split(" ").map { it.toInt() }.toIntArray()
-    val dp = Array(n1 + 1) { BooleanArray(30_001) }
+    val arr2 = readln().split(" ").map { it.toInt() }
+    var ms = mutableSetOf<Int>()
     val answer = mutableListOf<String>()
 
     for (i in 0 until n1) {
-        for (j in 0..30_000) {
-            if (dp[i][j]) {
-                dp[i + 1][j] = true
-                if (j + arr1[i] in 0..30_000) dp[i + 1][j + arr1[i]] = true
-                if (j - arr1[i] in 0..30_000) dp[i + 1][j - arr1[i]] = true
-            }
+        val nms = mutableSetOf<Int>()
+        ms.forEach {
+            nms.add(it)
+            nms.add(it + arr1[i])
+            nms.add(it - arr1[i])
         }
-
-        dp[i + 1][15_000 + arr1[i]] = true
-        dp[i + 1][15_000 - arr1[i]] = true
+        nms.add(arr1[i])
+        nms.add(-arr1[i])
+        ms = nms
     }
 
-    arr2.forEach { answer.add(if (it > 15_000) "N" else if (dp.last()[15_000 + it]) "Y" else "N") }
+    arr2.forEach { answer.add(if (ms.contains(it)) "Y" else "N") }
 
     println(answer.joinToString(" "))
 }
