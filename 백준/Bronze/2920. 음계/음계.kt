091fea209fe81fc4a45c6c2f021cf487a87cc298
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-04-23 11:01:51 +0900

    [Bronze II] Title: 음계, Time: 88 ms, Memory: 12284 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.kt
@@ -0,0 +1,9 @@
+fun main() {
+    println(
+        when (readln()) {
+            "1 2 3 4 5 6 7 8" -> "ascending"
+            "8 7 6 5 4 3 2 1" -> "descending"
+            else -> "mixed"
+        }
+    )
+}
