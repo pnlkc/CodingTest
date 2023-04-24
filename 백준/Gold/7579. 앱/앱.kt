9b3ec00e65d9cee297226b56b0c4eaeed46dc496
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-04-24 10:27:05 +0900

    [Gold III] Title: 앱, Time: 292 ms, Memory: 61684 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.kt
+++ 백준/Gold/7579. 앱/앱.kt
@@ -2,21 +2,18 @@ fun main() {
     val (n, m) = readln().split(" ").map { it.toInt() }
     val arrM = readln().split(" ").map { it.toInt() }
     val arrC = readln().split(" ").map { it.toInt() }
-    var dp = IntArray(100_001) { -1 }
+    var dp = IntArray(100_001)
     dp[arrC[0]] = arrM[0]
 
     for (i in 1 until n) {
         val nDP = dp.clone()
 
         for (j in dp.indices) {
-            if (dp[j] == -1) continue
-
-            if (nDP[j + arrC[i]] == -1) nDP[j + arrC[i]] = dp[j] + arrM[i]
-            else nDP[j + arrC[i]] = maxOf(nDP[j + arrC[i]], dp[j] + arrM[i])
+            if (dp[j] == 0) continue
+            nDP[j + arrC[i]] = maxOf(nDP[j + arrC[i]], dp[j] + arrM[i])
         }
-
-        if (nDP[arrC[i]] == -1) nDP[arrC[i]] = arrM[i]
-        else nDP[arrC[i]] = maxOf(nDP[arrC[i]], arrM[i])
+        
+        nDP[arrC[i]] = maxOf(nDP[arrC[i]], arrM[i])
 
         dp = nDP
     }
