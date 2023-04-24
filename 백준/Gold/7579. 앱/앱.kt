51adc4572f41d1ed2ab9763eadf3ed6639d4a511
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-04-24 10:21:33 +0900

    [Gold III] Title: 앱, Time: 328 ms, Memory: 62272 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.kt
+++ 백준/Gold/7579. 앱/앱.kt
@@ -6,16 +6,13 @@ fun main() {
     dp[arrC[0]] = arrM[0]
 
     for (i in 1 until n) {
-        val nDP = IntArray(100_001) { -1 }
+        val nDP = dp.clone()
 
         for (j in dp.indices) {
             if (dp[j] == -1) continue
 
             if (nDP[j + arrC[i]] == -1) nDP[j + arrC[i]] = dp[j] + arrM[i]
             else nDP[j + arrC[i]] = maxOf(nDP[j + arrC[i]], dp[j] + arrM[i])
-
-            if (nDP[j] == -1) nDP[j] = dp[j]
-            else nDP[j] = maxOf(nDP[j], dp[j])
         }
 
         if (nDP[arrC[i]] == -1) nDP[arrC[i]] = arrM[i]
@@ -24,10 +21,5 @@ fun main() {
         dp = nDP
     }
 
-    for (i in dp.indices) {
-        if (dp[i] >= m) {
-            println(i)
-            break
-        }
-    }
+    dp.forEachIndexed { idx, i -> if (i >= m) return println(idx) }
 }
