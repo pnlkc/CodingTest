d96802b19e3297b7739e86487e7147e94105275b
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-04-26 11:10:50 +0900

    [Gold III] Title: 오등큰수, Time: 1416 ms, Memory: 256364 KB -BaekjoonHub

--- 백준/Gold/17299. 오등큰수/오등큰수.kt
+++ 백준/Gold/17299. 오등큰수/오등큰수.kt
@@ -6,9 +6,7 @@ fun main() {
     val ints = IntArray(1_000_001)
     val n = readln().toInt()
     val st = StringTokenizer(readln())
-    val a = IntArray(n) {
-        st.nextToken().toInt().also { num -> ints[num]++ }
-    }
+    val a = IntArray(n) { st.nextToken().toInt().also { num -> ints[num]++ } }
 
     for (i in 0 until n) {
         while (stack.isNotEmpty()) {
