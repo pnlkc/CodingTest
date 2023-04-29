95622daa6b30ed2cbca76013ba80856ff27f8ca3
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-04-29 10:27:08 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 164 ms, Memory: 22100 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.kt
@@ -0,0 +1,42 @@
+import java.util.PriorityQueue
+
+fun main() {
+    val (n, m) = readln().split(" ").map(String::toInt)
+    val graph = Array(101) { (it + 1..it + 6).toMutableList() }
+    val isSpecial = BooleanArray(101)
+    val queue = PriorityQueue<Pair<Int, Int>>(compareBy { it.second })
+    val visit = IntArray(101) { -1 }
+
+    repeat(n + m) {
+        val (x, y) = readln().split(" ").map(String::toInt)
+        graph[x] = mutableListOf(y)
+        isSpecial[x] = true
+    }
+
+    queue.add(1 to 0)
+    visit[1] = 0
+
+    while (queue.isNotEmpty()) {
+        val (c, d) = queue.poll()!!
+
+        for (i in graph[c]) {
+            if (i > 100) continue
+
+            if (visit[i] == -1 || visit[i] > d + 1) {
+                if (isSpecial[i]) {
+                    if (visit[graph[i].first()] == -1 || visit[graph[i].first()] > d + 1) {
+                        visit[graph[i].first()] = d + 1
+                        queue.add(graph[i].first() to d + 1)
+                    }
+                } else {
+                    queue.add(i to d + 1)
+                    visit[i] = d + 1
+                }
+            }
+
+            if (i == 100) break
+        }
+    }
+    
+    println(visit[100])
+}
