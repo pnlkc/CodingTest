a1062101c0b99a7a3eb8b601c650436d9ca0e853
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-04-30 12:26:42 +0900

    [Silver V] Title: 집합, Time: 1160 ms, Memory: 314180 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.kt
+++ 백준/Silver/11723. 집합/집합.kt
@@ -1,14 +1,18 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
 fun main() {
     val sb = StringBuilder()
+    val br = BufferedReader(InputStreamReader(System.`in`))
     var bit = 0
 
-    repeat(readln().toInt()) {
-        val c = readln().split(" ")
+    repeat(br.readLine().toInt()) {
+        val c = br.readLine().split(" ")
 
         when (c[0]) {
             "add" -> bit = bit or (1 shl (c[1].toInt() - 1))
             "remove" -> bit = bit and (1 shl (c[1].toInt() - 1)).inv()
-            "check" -> sb.append(if (bit and (1 shl (c[1].toInt() - 1)) == 0) "0\n" else "1\n")
+            "check" -> sb.appendLine(if (bit and (1 shl (c[1].toInt() - 1)) == 0) 0 else 1)
             "toggle" -> bit = bit xor (1 shl (c[1].toInt() - 1))
             "all" -> bit = 0b11111111111111111111
             "empty" -> bit = 0
