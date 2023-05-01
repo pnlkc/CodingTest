16a9855cb693ddce6df68876ce7b23f3a2285232
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-05-01 11:03:49 +0900

    [Gold IV] Title: 이분 그래프, Time: 2068 ms, Memory: 317076 KB -BaekjoonHub

---
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.kt
@@ -0,0 +1,55 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.LinkedList
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+
+    repeat(br.readLine().toInt()) {
+        val (v, e) = br.readLine().split(" ").map { it.toInt() }
+        val graph = Array(v + 1) { mutableListOf<Int>() }
+
+        for(i in 1..e) {
+            val (a, b) = br.readLine().split(" ").map { it.toInt() }
+            graph[a].add(b)
+            graph[b].add(a)
+        }
+
+        val isVisit = BooleanArray(v + 1)
+        var r = true
+
+        p@for (i in 1..v) {
+            if (graph[i].size == 0) {
+                isVisit[i] = true
+                continue
+            }
+            if (isVisit[i]) continue
+
+            val nodeColor = IntArray(v + 1) { -1 }
+            val queue = LinkedList<Pair<Int, Int>>()
+
+            queue.add(i to 0)
+            isVisit[i] = true
+            nodeColor[i] = 0
+
+            while (queue.isNotEmpty()) {
+                val (node, color) = queue.poll()!!
+
+                for (j in graph[node]) {
+                    if (isVisit[j]) {
+                        if (nodeColor[j] == color) {
+                            r = false
+                            break@p
+                        }
+                    } else {
+                        queue.add(j to color.inv())
+                        isVisit[j] = true
+                        nodeColor[j] = color.inv()
+                    }
+                }
+            }
+        }
+
+        println(if (r) "YES" else "NO")
+    }
+}
