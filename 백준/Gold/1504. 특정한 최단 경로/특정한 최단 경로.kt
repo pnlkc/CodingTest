b805120e478a84a7e6d38246b1c19e0278374db5
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-05-03 10:56:11 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 1116 ms, Memory: 125072 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.kt
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.kt
@@ -3,9 +3,7 @@ import java.util.PriorityQueue
 fun main() {
     val (n, e) = readln().split(' ').map(String::toInt)
     val graph = Array(n + 1) { mutableListOf<Pair<Int, Int>>() }
-    val pQueue = PriorityQueue<Pair<Int, Int>>(compareBy { it.second })
-    val dist = Array(3) { IntArray(n + 1) { -1 } }
-
+    
     repeat(e) {
         val (a, b, c) = readln().split(' ').map(String::toInt)
         graph[a].add(b to c)
@@ -13,32 +11,38 @@ fun main() {
     }
 
     val (v1, v2) = readln().split(' ').map(String::toInt)
-    val arr = intArrayOf(1, v1, v2)
+    val dist1 = dijkstra(1, n, graph)
+    val distV1 = dijkstra(v1, n, graph)
+    val distV2 = dijkstra(v2, n, graph)
+    val v1v2 = intArrayOf(dist1[v1], distV1[v2], distV2[n])
+    val v2v1 = intArrayOf(dist1[v2], distV2[v1], distV1[n])
 
-    for (i in dist.indices) {
-        pQueue.add(arr[i] to 0)
-        dist[i][arr[i]] = 0
-
-        while (pQueue.isNotEmpty()) {
-            val (cNode, cDist) = pQueue.poll()!!
-
-            if (dist[i][cNode] != -1 && cDist > dist[i][cNode]) continue
-
-            for ((jNode, jDist) in graph[cNode]) {
-                if (dist[i][jNode] == -1 || dist[i][jNode] > cDist + jDist) {
-                    dist[i][jNode] = dist[i][cNode] + jDist
-                    pQueue.add(jNode to dist[i][jNode])
-                }
-            }
-        }
-    }
-    
-    val v1v2 = intArrayOf(dist[0][v1], dist[1][v2], dist[2][n])
-    val v2v1 = intArrayOf(dist[0][v2], dist[2][v1], dist[1][n])
-    
     println(
         intArrayOf(if (v1v2.contains(-1)) -1 else v1v2.sum(), if (v2v1.contains(-1)) -1 else v2v1.sum() )
             .filter { it > 0 }
             .minOrNull() ?: -1
     )
+}
+
+fun dijkstra(i: Int, n: Int, graph: Array<MutableList<Pair<Int,Int>>>): IntArray {
+    val pQueue = PriorityQueue<Pair<Int, Int>>(compareBy { it.second })
+    val dist = IntArray(n + 1) { -1 }
+
+    pQueue.add(i to 0)
+    dist[i] = 0
+
+    while (pQueue.isNotEmpty()) {
+        val (cNode, cDist) = pQueue.poll()!!
+
+        if (dist[cNode] != -1 && cDist > dist[cNode]) continue
+
+        for ((jNode, jDist) in graph[cNode]) {
+            if (dist[jNode] == -1 || dist[jNode] > cDist + jDist) {
+                dist[jNode] = dist[cNode] + jDist
+                pQueue.add(jNode to dist[jNode])
+            }
+        }
+    }
+    
+    return dist
 }
