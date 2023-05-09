a25673b91626727f5c46a6eb72b39bb56c593901
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-05-09 10:02:05 +0900

    [Gold V] Title: 두 용액, Time: 484 ms, Memory: 38792 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.kt
@@ -0,0 +1,25 @@
+import java.util.StringTokenizer
+import kotlin.math.abs
+
+fun main() {
+    val n = readln().toInt()
+    val st = StringTokenizer(readln())
+    val list = List(n) { st.nextToken().toInt() }.sorted()
+    var diff = Int.MAX_VALUE
+    var min = intArrayOf(0, 0)
+    var s = 0
+    var e = n - 1
+
+    while (s < e) {
+        if (diff > abs(list[s] + list[e])) {
+            diff = abs(list[s] + list[e])
+            min = intArrayOf(list[s], list[e])
+            if (abs(list[s] + list[e]) == 0) return println("${list[s]} ${list[e]}")
+        }
+
+        if (list[s] + list[e] < 0) s++
+        else e--
+    }
+
+    println(min.joinToString(" "))
+}
