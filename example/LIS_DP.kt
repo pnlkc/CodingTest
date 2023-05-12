9e566c56df9422579b8f933c9b538fd95cce583c
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-05-12 10:56:16 +0900

    Create LIS_DP.kt

---
+++ example/LIS_DP.kt
@@ -0,0 +1,20 @@
+// LIS(Longest Increasing Subsequence) 알고리즘은 가장 긴 증가하는 부분 수열을 찾는 알고리즘입니다.
+// LIS 알고리즘은 DP 기반의 알고리즘입니다.
+// ex) 백준 11053번 가장 긴 증가하는 부분 수열
+class LIS_DP {
+    fun example() {
+        val num = readln().toInt()
+        val arr = readln().split(" ").map { it.toInt() }
+        val memo = IntArray(num) { 1 }
+        var max = 1
+
+        for (i in 1 until num) {
+            for (j in 0 until i) {
+                if (arr[i] > arr[j]) memo[i] = maxOf(memo[i], memo[j] + 1)
+            }
+            max = maxOf(max, memo[i])
+        }
+
+        println(max)
+    }
+}
