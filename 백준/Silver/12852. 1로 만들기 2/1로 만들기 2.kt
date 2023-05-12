ce4877379a4cfff3b48d1d083074c84091cbbcbd
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-05-12 10:04:10 +0900

    [Silver I] Title: 1로 만들기 2, Time: 1720 ms, Memory: 748528 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.kt
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.kt
@@ -2,29 +2,26 @@ import java.util.PriorityQueue
 
 fun main() {
     val n = readln().toInt()
-    val pQ = PriorityQueue(compareBy<Triple<Int, Int, MutableList<Int>>> { it.second })
-    pQ.add(Triple(n, 0, mutableListOf(n)))
+    val pQ = PriorityQueue(compareBy<Triple<Int, Int, String>> { it.second })
+    pQ.add(Triple(n, 0, "$n"))
     if (n == 1) return println("0\n1")
 
     while (pQ.isNotEmpty()) {
-        val (num, count, mList) = pQ.poll()!!
+        val (num, count, s) = pQ.poll()!!
 
         if (num % 3 == 0) {
-            val temp = mList.toMutableList().also { it.add(num / 3) }
-            if (num / 3 == 1) return println("${count + 1}\n${temp.joinToString(" ")}")
-            pQ.add(Triple(num / 3, count + 1, temp))
+            if (num / 3 == 1) return println("${count + 1}\n${s + " ${num / 3}"}")
+            pQ.add(Triple(num / 3, count + 1, s + " ${num / 3}"))
         }
 
         if (num % 2 == 0) {
-            val temp = mList.toMutableList().also { it.add(num / 2) }
-            if (num / 2 == 1) return println("${count + 1}\n${temp.joinToString(" ")}")
-            pQ.add(Triple(num / 2, count + 1, temp))
+            if (num / 2 == 1) return println("${count + 1}\n${s + " ${num / 2}"}")
+            pQ.add(Triple(num / 2, count + 1, s + " ${num / 2}"))
         }
 
         if (num > 1) {
-            val temp = mList.toMutableList().also { it.add(num - 1) }
-            if (num - 1 == 1) return println("${count + 1}\n${temp.joinToString(" ")}")
-            pQ.add(Triple(num - 1, count + 1, temp))
+            if (num - 1 == 1) return println("${count + 1}\n${s + " ${num - 1}"}")
+            pQ.add(Triple(num - 1, count + 1, s + " ${num - 1}"))
         }
     }
 }
