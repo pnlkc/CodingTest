8f52af6e0070292c32f167df9327821222947916
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-05-14 11:16:36 +0900

    [Bronze II] Title: 농구 경기, Time: 112 ms, Memory: 14588 KB -BaekjoonHub

--- 백준/Bronze/1159. 농구 경기/농구 경기.kt
+++ 백준/Bronze/1159. 농구 경기/농구 경기.kt
@@ -1,12 +1,9 @@
 fun main() {
-    val map = ('a'..'z').map { it to 0 }.toMap().toMutableMap()
-    val sb = StringBuilder()
+    val arr = List(readln().toInt()) { readln() }
     
-    repeat(readln().toInt()) {
-        val s = readln()
-        map[s[0]] = map[s[0]]!! + 1
-    }
-    
-    map.forEach { c, i -> if (i >= 5) sb.append(c) }
-    println (sb.ifEmpty { "PREDAJA" })
+    println(
+        ('a'..'z')
+            .fold("") { acc, c -> if (arr.count { it[0] == c } >= 5) acc + c else acc }
+            .ifEmpty { "PREDAJA" }
+    )
 }
