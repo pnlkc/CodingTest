01e53a83da69a1c086e0f5d2bac3880483560a0d
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-05-17 09:58:54 +0900

    [Bronze II] Title: 운동, Time: 136 ms, Memory: 21288 KB -BaekjoonHub

--- 백준/Bronze/1173. 운동/운동.kt
+++ 백준/Bronze/1173. 운동/운동.kt
@@ -1,12 +1,13 @@
 fun main() {
-    val input = readln().split(' ').map(String::toInt).toIntArray()
+    val input = readln().split(' ').map(String::toInt)
+    var et = 0
     var t = 0
     var c = input[1]
 
-    while (input[0] > 0) {
+    while (et < input[0]) {
         if (c + input[3] <= input[2]) {
             c += input[3]
-            input[0]--
+            et++
             t++
         } else {
             if (c == input[1]) return println(-1)
