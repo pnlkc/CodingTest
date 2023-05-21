638876f0dd0e02927ba431052cd3c2f0ce3edb7b
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-05-21 10:44:52 +0900

    [Bronze II] Title: 이상한 곱셈, Time: 172 ms, Memory: 22864 KB -BaekjoonHub

--- 백준/Bronze/1225. 이상한 곱셈/이상한 곱셈.kt
+++ 백준/Bronze/1225. 이상한 곱셈/이상한 곱셈.kt
@@ -1,6 +1,4 @@
 fun main() {
-    val (a, b) = readln().split(' ')
-    val aSum = a.map(Char::digitToInt).sum()
-    val bSum = b.map(Char::digitToInt).sum()
-    println(aSum.toLong() * bSum)
+    val (a, b) = readln().split(' ').map { it.sumOf { c -> c.digitToInt() } }
+    println(a.toLong() * b)
 }
