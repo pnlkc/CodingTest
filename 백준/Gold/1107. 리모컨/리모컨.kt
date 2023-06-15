4a37f68bffb0a197dd6dca6e5c315985bf1639d9
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-06-15 11:37:00 +0900

    [Gold V] Title: 리모컨, Time: 404 ms, Memory: 87740 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.kt
+++ 백준/Gold/1107. 리모컨/리모컨.kt
@@ -1,33 +1,23 @@
 import kotlin.math.abs
 
 fun main() {
-    val n = readln()
+    val n = readln().toInt()
     val m = readln().toInt()
 
     val rN = if(m == 0) listOf<Char>() else readln().split(' ').map { it[0] }
     var num = 0
     var length = Int.MAX_VALUE
 
-    for (i in 0..abs(n.toInt() - 100)) {
-        val minus = (n.toInt() - i).toString()
-        val plus = (n.toInt() + i).toString()
-
-        when {
-            check(minus, rN) && check(plus, rN) -> {
-                num = i
-                length = minOf((n.toInt() - i).toString().length, (n.toInt() + i).toString().length)
-                break
-            }
-            check(minus, rN) -> {
-                num = i
-                length = (n.toInt() - i).toString().length
-                break
-            }
-            check(plus, rN) -> {
-                num = i
-                length = (n.toInt() + i).toString().length
-                break
-            }
+    for (i in 0..abs(n - 100)) {
+        val minus = (n - i).toString()
+        val plus = (n + i).toString()
+        val result = booleanArrayOf(check(minus, rN), check(plus, rN))
+        
+        if (result.contains(true)) {
+            num = i
+            if (result[0]) length = minus.length
+            if (result[1]) length = minOf(length, plus.length)
+            break
         }
     }
 
