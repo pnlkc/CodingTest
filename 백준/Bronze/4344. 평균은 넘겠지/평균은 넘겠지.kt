53a7784728fd8382ca5d02f3ecc7c649271ea84b
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-06-28 18:14:24 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 228 ms, Memory: 18660 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.kt
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.kt
@@ -1,14 +1,18 @@
-import java.text.DecimalFormat
-
 fun main() {
     val testNum = readln().toInt()
 
     repeat(testNum) {
         val input = readln().split(" ").map { it.toInt() }
-        val slice = input.slice(1..input.lastIndex)
-        val avg = slice.sum() / input[0]
-        val num = slice.count { it > avg }
-        val result = num.toDouble() / input[0] * 100
-        println(DecimalFormat("##0.000").format(result) + "%")
+        val list = input.slice(1..input.lastIndex)
+        val sum = list.sum()
+        val count = list.count { it * list.size > sum }
+        var result = count * 1_000_000 / list.size
+        if (result % 10 >= 5) result += 10
+        val answer = ((result / 10).toDouble() / 1_000).toString().split('.')
+        var back = answer[1]
+        repeat(3 - answer[1].length) {
+            back += "0"
+        }
+        println(answer[0] + "." + back + "%")
     }
 }
