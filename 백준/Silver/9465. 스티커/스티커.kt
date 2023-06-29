bd1a9790d4c2fc5c9d9e83f1851f8232793dfadb
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-06-29 18:10:41 +0900

    [Silver I] Title: 스티커, Time: 1012 ms, Memory: 143952 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.kt
@@ -0,0 +1,21 @@
+fun main() {
+    val sb = StringBuilder()
+    val tcNum = readln().toInt()
+
+    repeat(tcNum) {
+        val n = readln().toInt()
+        val arr = Array(2) { listOf<Int>() }
+        val dp = Array(2) { IntArray(n + 2) }
+
+        (0..1).forEach { arr[it] = readln().split(' ').map(String::toInt) }
+
+        for (i in 0 until n) {
+            dp[0][i + 2] = maxOf(dp[1][i + 1] + arr[0][i], dp[0][i] + arr[0][i], dp[1][i] + arr[0][i])
+            dp[1][i + 2] = maxOf(dp[0][i + 1] + arr[1][i], dp[0][i] + arr[1][i], dp[1][i] + arr[1][i])
+        }
+
+        sb.appendLine(maxOf(dp[0][n + 1], dp[1][n + 1]))
+    }
+
+    println(sb)
+}
