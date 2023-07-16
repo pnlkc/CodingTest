76fa40d32febe5ef8cb9a801b4325b8b55e36c17
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-07-17 08:29:44 +0900

    [Bronze II] Title: 직사각형을 만드는 방법, Time: 1504 ms, Memory: 12636 KB -BaekjoonHub

---
+++ 백준/Bronze/8320. 직사각형을 만드는 방법/직사각형을 만드는 방법.kt
@@ -0,0 +1,17 @@
+import java.math.*
+
+fun main() {
+    val n = readln().toInt()
+    var count = 0
+    
+    for (k in 1..n) {
+        val sqrt = Math.sqrt(k.toDouble()).toInt()
+        for (i in 1..sqrt) {
+            for (j in sqrt..k) {
+                if (i * j == k) count++
+            }
+        }
+    }
+    
+    println(count)
+}
