c057010bf419cd6ec8cf125d8baa993427e959cb
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-07-24 16:04:47 +0900

    [D1] Title: 큰 놈, 작은 놈, 같은 놈, Time: 109 ms, Memory: 18,264 KB -BaekjoonHub

---
+++ SWEA/D1/2070. 큰 놈， 작은 놈， 같은 놈/큰 놈， 작은 놈， 같은 놈.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int tc = Integer.parseInt(br.readLine());
+		
+		for (int t = 1; t <= tc; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			if (a == b) {
+				System.out.println("#" + t + " " + "=");
+			} else if (a > b) {
+				System.out.println("#" + t + " " + ">");
+			} else {
+				System.out.println("#" + t + " " + "<");
+			}
+		}
+	}
+}
