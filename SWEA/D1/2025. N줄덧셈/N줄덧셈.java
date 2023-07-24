79ae8ca6958bdfc8f89b723cc06e22942f46b589
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-07-24 14:57:32 +0900

    [D1] Title: N줄덧셈, Time: 102 ms, Memory: 18,352 KB -BaekjoonHub

---
+++ SWEA/D1/2025. N줄덧셈/N줄덧셈.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int answer = 0;
+		
+		for (int i = 1; i <= n; i++) {
+			answer += i;
+		}
+		
+		System.out.println(answer);
+	}
+}
