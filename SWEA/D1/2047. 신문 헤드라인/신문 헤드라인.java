3d7519ee6fa5c2bb6495b15a95e0617946e799b6
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-07-24 15:09:26 +0900

    [D1] Title: 신문 헤드라인, Time: 100 ms, Memory: 18,392 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		System.out.println(input.toUpperCase());
+	}
+}
