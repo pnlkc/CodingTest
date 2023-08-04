8df868d15d388137fc57bc6d4e7b3fd18e0984a9
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-08-05 00:17:39 +0900

    [Gold III] Title: 웜홀, Time: 1088 ms, Memory: 45024 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.java
+++ 백준/Gold/1865. 웜홀/웜홀.java
@@ -1,22 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
 
+class Node {
+	int e, d;
+
+	public Node(int e, int d) {
+		this.e = e;
+		this.d = d;
+	}
+}
+
 public class Main {
+	static int n;
+	static List<List<Node>> graph;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int tcNum = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
 
 		p: for (int tc = 0; tc < tcNum; tc++) {
 			StringTokenizer st1 = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st1.nextToken());
+			n = Integer.parseInt(st1.nextToken());
 			int m = Integer.parseInt(st1.nextToken());
 			int w = Integer.parseInt(st1.nextToken());
-			Map<Integer, Integer>[] graph = new HashMap[n + 1];
+			graph = new ArrayList<>();
+			for (int i = 0; i < n + 1; i++) {
+				graph.add(new ArrayList<>());
+			}
 
 			for (int i = 0; i < m + w; i++) {
 				StringTokenizer st2 = new StringTokenizer(br.readLine());
@@ -24,59 +42,55 @@ public class Main {
 				int e = Integer.parseInt(st2.nextToken());
 				int t = Integer.parseInt(st2.nextToken());
 
-				if (i < m) {	
-					if (graph[s] == null) graph[s] = new HashMap<>();
-
-					if (graph[s].get(e) == null) graph[s].put(e, t);
-					else graph[s].put(e, Math.min(graph[s].get(e), t));
-					
-					if (graph[e] == null) graph[e] = new HashMap<>();
-
-					if (graph[e].get(s) == null) graph[e].put(s, t);
-					else graph[e].put(s, Math.min(graph[e].get(s), t));
+				if (i < m) {
+					graph.get(s).add(new Node(e, t));
+					graph.get(e).add(new Node(s, t));
 				} else {
-					if (graph[s] == null) graph[s] = new HashMap<>();
-
-					if (graph[s].get(e) == null) graph[s].put(e, t * -1);
-					else graph[s].put(e, Math.min(graph[s].get(e), t * -1));
+					graph.get(s).add(new Node(e, -t));
 				}
 			}
 
-			if (bellmanford(n, graph)) {
-				System.out.println("YES");
-				continue p;
+			for (int i = 1; i <= n; i++) {
+				if (bellmanford(i)) {
+					sb.append("YES").append("\n");
+					continue p;
+				}
 			}
-			
-			System.out.println("NO");
+
+			sb.append("NO").append("\n");
 		}
+
+		System.out.println(sb);
 	}
 
-	public static boolean bellmanford(int n, Map<Integer, Integer>[] graph) {
+	public static boolean bellmanford(int s) {
 		int[] dist = new int[n + 1];
-		dist[1] = 0;
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		dist[s] = 0;
 
 		for (int i = 0; i < n - 1; i++) {
+			boolean isChange = false;
+			
 			for (int j = 1; j <= n; j++) {
-				if (graph[j] != null) {
-					for (int e : graph[j].keySet()) {
-						if (dist[j] + graph[j].get(e) < dist[e]) {
-							dist[e] = dist[j] + graph[j].get(e);
-						}
+				for (Node node : graph.get(j)) {
+					if (dist[j] != Integer.MAX_VALUE && dist[node.e] > dist[j] + node.d) {
+						dist[node.e] = dist[j] + node.d;
+						isChange = true;
 					}
 				}
 			}
+			
+			if (!isChange) break;
 		}
 
 		for (int i = 1; i <= n; i++) {
-			if (graph[i] != null) {
-				for (int e : graph[i].keySet()) {
-					if (dist[e] > dist[i] + graph[i].get(e)) {
-						return true;
-					}
+			for (Node node : graph.get(i)) {
+				if (dist[i] != Integer.MAX_VALUE && dist[node.e] > dist[i] + node.d) {
+					return true;
 				}
 			}
 		}
-		
+
 		return false;
 	}
 }
