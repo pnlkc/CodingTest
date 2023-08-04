5dbb4544d2bd9f2d1555cbfb65ded56d8b1730ac
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-08-05 00:24:19 +0900

    [Gold IV] Title: 타임머신, Time: 424 ms, Memory: 34336 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.kt
+++ 백준/Gold/11657. 타임머신/타임머신.kt
@@ -1,39 +1,44 @@
-fun main() {
-    val (n, m) = readln().split(' ').map(String::toInt)
-    val graph = Array(n + 1) { mutableMapOf<Int, Int>() }
 
-    repeat(m) {
-        val (a, b, c) = readln().split(' ').map(String::toInt)
-        if (graph[a][b] == null) graph[a][b] = c else graph[a][b] = minOf(c, graph[a][b]!!)
+    fun main() {
+        val (n, m) = readln().split(' ').map(String::toInt)
+        val graph = Array(n + 1) { mutableMapOf<Int, Int>() }
+
+        repeat(m) {
+            val (a, b, c) = readln().split(' ').map(String::toInt)
+            if (graph[a][b] == null) graph[a][b] = c else graph[a][b] = minOf(c, graph[a][b]!!)
+        }
+
+        val (result, arr) = bellmanFord(n, graph)
+
+        if (result) arr.forEachIndexed { idx, i -> if (idx > 1) println(if (i != Long.MAX_VALUE) i else -1) }
+        else println(-1)
     }
 
-    val (result, arr) = calc(n, m, graph)
+    fun bellmanFord(n: Int, graph: Array<MutableMap<Int, Int>>): Pair<Boolean, LongArray> {
+        val dist = LongArray(n + 1) { Long.MAX_VALUE }
+        dist[1] = 0
+        
+        repeat(n - 1) {
+            var isChange = false
+            for (i in 1..n) {
+                for ((cn, cd) in graph[i]) {
+                    if (dist[i] != Long.MAX_VALUE && dist[i] + cd < dist[cn]) {
+                        dist[cn] = dist[i] + cd
+                        isChange = true
+                    }
+                }
+            }
 
-    if (result) arr.forEachIndexed { idx, i -> if (idx > 1) println(if (i != Long.MAX_VALUE) i else -1) }
-    else println(-1)
-}
+            if (!isChange) return true to dist
+        }
 
-fun calc(n: Int, m: Int, graph: Array<MutableMap<Int, Int>>): Pair<Boolean, LongArray> {
-    val dist = LongArray(n + 1) { Long.MAX_VALUE }
-    dist[1] = 0
-
-    repeat(n - 1) {
         for (i in 1..n) {
             for ((cn, cd) in graph[i]) {
-                if (dist[i] != Long.MAX_VALUE && dist[i] + cd < dist[cn]) {
-                    dist[cn] = dist[i] + cd
+                if (dist[cn] != Long.MAX_VALUE && dist[i] + cd < dist[cn]) {
+                    return false to longArrayOf()
                 }
             }
         }
-    }
 
-    for (i in 1..n) {
-        for ((cn, cd) in graph[i]) {
-            if (dist[cn] != Long.MAX_VALUE && dist[i] + cd < dist[cn]) {
-                return false to longArrayOf()
-            }
-        }
+        return true to dist
     }
-
-    return true to dist
-}
