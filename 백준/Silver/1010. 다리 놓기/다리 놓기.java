2c7879726a9ae75ba844186b39ea6f67c18f4bcd
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-08-07 14:30:47 +0900

    [Silver V] Title: 다리 놓기, Time: 124 ms, Memory: 12448 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int t = 0; t < T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			int sum = 1;
+			for (int i = 1; i <= N; i++) {
+				sum = sum * (M - N + i) / i;
+			}
+			System.out.println(sum);
+		}
+		
+	}
+}
