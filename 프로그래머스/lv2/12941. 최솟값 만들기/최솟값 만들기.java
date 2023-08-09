e01c92c57bd7c49bb4d0e8031777c755017bac1d
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-08-09 15:57:56 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.02 ms, Memory: 51.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+    	int sum = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        for (int i = 0; i < A.length; i++) {
+        	sum += A[i] * B[B.length - 1 - i];
+        }
+        return sum;
+    }
+}
