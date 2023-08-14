a6c060a0c2602e4e82a2cdcbb403f0a34a0f6f1b
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-08-14 14:46:39 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 124 ms, Memory: 13252 KB -BaekjoonHub

---
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		ArrayDeque<int[]> deque = new ArrayDeque<>();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		for (int i = 1; i <= N; i++) {
+			deque.offerLast(new int[] { i, Integer.parseInt(st.nextToken()) });
+		}
+
+		for (int i = 0; i < N; i++) {
+			int[] c = deque.pollFirst();
+			int num = c[1];
+			sb.append(c[0] + " ");
+			if (i == N - 1)
+				break;
+
+			if (num < 0) {
+				for (int j = 0; j < -num; j++) {
+					deque.offerFirst(deque.pollLast());
+				}
+			} else {
+				for (int j = 0; j < num - 1; j++) {
+					deque.offerLast(deque.pollFirst());
+				}
+			}
+		}
+
+		System.out.println(sb.toString().trim());
+	}
+}
