d840beed781f6087fd072ad3b3b028082000be5f
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-08-14 16:58:16 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - String, Time: 103 ms, Memory: 18,388 KB -BaekjoonHub

--- SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
+++ SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
@@ -1,24 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		for (int tc = 1; tc <= 10; tc++) {
 			int t = Integer.parseInt(br.readLine());
 			String target = br.readLine();
 			String str = br.readLine();
+			Map<Character, Integer> map = new HashMap<>();
 			int cnt = 0;
 			
-			p: for (int i = 0; i <= str.length() - target.length(); i++) {
-				if (str.charAt(i) == target.charAt(0)) {
-					for (int j = 0; j < target.length(); j++) {
-						if (str.charAt(i + j) != target.charAt(j)) {
+			for (int i = 0; i < target.length(); i++) {
+				map.put(target.charAt(target.length() - 1 - i), i);
+			}
+			
+			int idx = 0;
+			
+			p: while (idx < str.length()) {
+				if (map.get(str.charAt(idx)) == null) {
+					idx += target.length();
+				} else if (str.charAt(idx) == target.charAt(target.length() - 1)) {
+					for (int i = 0; i < target.length(); i++) {
+						if (str.charAt(idx - i) != target.charAt(target.length() - 1 - i)) {
+							idx += target.length();
 							continue p;
 						}
 					}
+					idx += target.length();
 					cnt++;
+				} else {
+					idx += map.get(str.charAt(idx));
 				}
 			}
 			
