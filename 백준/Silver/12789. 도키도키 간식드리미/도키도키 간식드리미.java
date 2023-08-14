862b4ec957dfd3d2ca8b56ca6240670ac726ab9a
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-08-14 12:20:41 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 88 ms, Memory: 11820 KB -BaekjoonHub

---
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int num = 1;
+		Stack<Integer> stack = new Stack<>();
+
+		for (int i = 0; i < N; i++) {
+			int next = Integer.parseInt(st.nextToken());
+			if (next != num) {
+				if (!stack.isEmpty() && stack.peek() == num) {
+					while (!stack.isEmpty() && stack.peek() == num) {
+						stack.pop();
+						num++;
+					}
+				}
+				if (next != num) stack.push(next);
+			} else {
+				num++;
+				if (!stack.isEmpty() && stack.peek() == num) {
+					while (!stack.isEmpty() && stack.peek() == num) {
+						stack.pop();
+						num++;
+					}
+				}
+			}
+		}
+		
+		if (stack.isEmpty()) {
+			System.out.println("Nice");
+		} else {
+			System.out.println("Sad");
+		}
+
+	}
+}
