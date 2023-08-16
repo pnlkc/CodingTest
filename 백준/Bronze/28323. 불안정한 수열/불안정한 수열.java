bfe6c0620a2ec973fc06165774be6931071bf8e5
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-08-16 11:43:15 +0900

    [Bronze II] Title: 불안정한 수열, Time: 156 ms, Memory: 18736 KB -BaekjoonHub

--- 백준/Bronze/28323. 불안정한 수열/불안정한 수열.java
+++ 백준/Bronze/28323. 불안정한 수열/불안정한 수열.java
@@ -8,34 +8,37 @@ import java.util.StringTokenizer;
 public class Main {
 	static int max = 0;
 	static int[] arr;
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		arr = new int[N];
-		
+
 		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		
+
 		for (int n = N; n >= 1; n--) {
-			calc(0, n, N, new ArrayList<>());
+			if (n > max) {
+				calc(0, n, N, new ArrayList<>());
+			}
 		}
-		
+
 		System.out.println(max);
 	}
-	
+
 	public static void calc(int i, int n, int N, List<Integer> used) {
-		if (n < max) return;
-		
+		if (n < max)
+			return;
+
 		if (used.size() == n) {
 			for (int idx = 0; idx < used.size() - 1; idx++) {
 				if ((used.get(idx) + used.get(idx + 1)) % 2 == 0) {
 					return;
 				}
 			}
-			
+
 			max = n;
 		} else {
 			for (int idx = i; idx < N; idx++) {
