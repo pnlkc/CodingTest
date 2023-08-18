a83768026ba4fd5b2cda297d1dca40efa2ef1420
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-08-18 11:42:06 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 584 ms, Memory: 235060 KB -BaekjoonHub

--- 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
@@ -3,39 +3,38 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	static int[][] arr;
-	
+	static StringBuilder sb = new StringBuilder();
+	static char[][] arr;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		int N = Integer.parseInt(br.readLine());
-		arr = new int[N][N * 2 - 1];
-		
-		calc(0, N - 1, N);
-		
+
+		arr = new char[N][2 * N - 1]; // 배열을 만들어서 관리하겠다.
+		m(N, 0, 0);
+		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N * 2 - 1; j++) {
-				if (arr[i][j] == 0) sb.append(" ");
-				else sb.append("*");
+			for (int j = 0; j < 2 * N - 1; j++) {
+				if (arr[i][j] == '*') sb.append("*");
+				else sb.append(" ");
 			}
 			sb.append("\n");
 		}
-		
 		System.out.println(sb);
 	}
-	
-	public static void calc(int x, int y, int n) {
-		if (n == 3) {
+
+	static void m(int N, int r, int c) { // N:크기, r:시작 행, c:시작 열
+		if (N == 3) {
+			arr[r][c + 2] = '*';
+			arr[r + 1][c + 1] = '*';
+			arr[r + 1][c + 3] = '*';
 			for (int i = 0; i < 5; i++) {
-				arr[y][x + i] = 1;
+				arr[r + 2][c + i] = '*';
 			}
-			arr[y - 1][x + 1] = 1;
-			arr[y - 1][x + 3] = 1;
-			arr[y - 2][x + 2] = 1;
 		} else {
-			calc(x, y, n / 2);
-			calc(x + n, y, n /2);
-			calc(x + n / 2, y - n / 2, n /2);
+			m(N / 2, r, c + N / 2);
+			m(N / 2, r + N / 2, c);
+			m(N / 2, r + N / 2, c + N);
 		}
 	}
 }
