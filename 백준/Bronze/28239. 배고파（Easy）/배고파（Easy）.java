59aedfb7aee920b7dceaa3bd6f969a6af4adc738
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-08-22 15:23:46 +0900

    [Bronze I] Title: 배고파(Easy), Time: 648 ms, Memory: 51240 KB -BaekjoonHub

--- 백준/Bronze/28239. 배고파（Easy）/배고파（Easy）.java
+++ 백준/Bronze/28239. 배고파（Easy）/배고파（Easy）.java
@@ -4,26 +4,34 @@ import java.io.InputStreamReader;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		StringBuilder sb = new StringBuilder();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		
-		p: for (int i = 0; i < n; i++) {
+		for (int i = 0; i < n; i++) {
 			long m = Long.parseLong(br.readLine());
+			String str = Long.toBinaryString(m);
+			int cnt = 0;
+			int a = -1;
+			int b = -1;
 			
-			for (int j = 0; j < 64; j++) {
-				long a = (long) Math.pow(2, (double) j);
-				
-				for (int k = j; k < 64; k++) {
-					long b = (long) Math.pow(2, (double) k);
-					if (a + b == m) {
-						sb.append(j + " " + k).append("\n");
-						continue p;
+			for (int j = 0; j < str.length(); j++) {
+				if (Integer.parseInt(String.valueOf(str.charAt(str.length() - 1 - j))) == 1) {
+					cnt++;
+					
+					if (cnt == 1) {
+						a = j;
+					} else {
+						b = j;
+						break;
 					}
 				}
 			}
+			
+			if (cnt == 1) {
+				System.out.println((a - 1) + " " + (a - 1));
+			} else {
+				System.out.println(a + " " + b);
+			}
 		}
-		
-		System.out.println(sb);
 	}
 }
