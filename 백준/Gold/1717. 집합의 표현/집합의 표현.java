40536754f1dfe8816a79384ed0f15214bac75fe5
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-08-29 15:49:13 +0900

    [Gold V] Title: 집합의 표현, Time: 1820 ms, Memory: 59488 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n, m;
+	static int[] parent, rank;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st1 = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st1.nextToken());
+		m = Integer.parseInt(st1.nextToken());
+		parent = new int[n + 1];
+		rank = new int[n + 1];
+		
+		for (int i = 0; i <= n; i++) {
+			parent[i] = i;
+		}
+		
+		for (int i = 0; i < m; i++) {
+			StringTokenizer st2 = new StringTokenizer(br.readLine());
+			int c = Integer.parseInt(st2.nextToken());
+			int a = Integer.parseInt(st2.nextToken());
+			int b = Integer.parseInt(st2.nextToken());
+			
+			if (c == 0) {
+				union(a, b);
+			} else {
+				if (find(a) == find(b)) {
+					System.out.println("YES");
+				} else {
+					System.out.println("NO");
+				}
+			}
+		}
+	}
+	
+	public static boolean union(int n1, int n2) {
+		int p1 = find(n1);
+		int p2 = find(n2);
+		
+		if (p1 == p2) return false;
+		
+		if (rank[p1] > rank[p2]) {
+			parent[p2] = p1;
+		} else if (rank[p1] < rank[p2]) {
+			parent[p1] = p2;
+		} else {
+			parent[p2] = p1;
+			rank[p1]++;
+		}
+		
+		return true;
+	}
+	
+	public static int find(int n) {
+		if (parent[n] != n) {
+			return parent[n] = find(parent[n]);
+		}
+		return parent[n];
+	}
+}
