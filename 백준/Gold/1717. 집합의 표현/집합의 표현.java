ebd190c7517039128f1f6af5db49b050f13f54a0
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-08-29 15:49:48 +0900

    [Gold V] Title: 집합의 표현, Time: 1708 ms, Memory: 58592 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -37,11 +37,11 @@ public class Main {
 		}
 	}
 	
-	public static boolean union(int n1, int n2) {
+	public static void union(int n1, int n2) {
 		int p1 = find(n1);
 		int p2 = find(n2);
 		
-		if (p1 == p2) return false;
+		if (p1 == p2) return;
 		
 		if (rank[p1] > rank[p2]) {
 			parent[p2] = p1;
@@ -51,8 +51,6 @@ public class Main {
 			parent[p2] = p1;
 			rank[p1]++;
 		}
-		
-		return true;
 	}
 	
 	public static int find(int n) {
