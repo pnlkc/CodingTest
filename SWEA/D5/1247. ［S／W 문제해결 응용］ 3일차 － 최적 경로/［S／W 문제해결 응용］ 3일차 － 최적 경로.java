923daf11f1b99089f016fcd29d08f3ecae0ca7df
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-08-30 16:20:15 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 최적 경로, Time: 477 ms, Memory: 20,940 KB -BaekjoonHub

--- SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
+++ SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 class P {
@@ -15,62 +13,67 @@ class P {
 }
 
 public class Solution {
-	static List<Integer> used;
+	static int[] pick;
 	static int[][] dist;
 	static int min;
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
-		
+
 		for (int tc = 1; tc <= T; tc++) {
-			used = new ArrayList<>();
-			min = Integer.MAX_VALUE;
 			int N = Integer.parseInt(br.readLine());
 			P[] arr = new P[N + 2];
+			pick = new int[N];
+			min = Integer.MAX_VALUE;
 			dist = new int[N + 2][N + 2];
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			
+
 			for (int i = 0; i < N + 2; i++) {
 				int x = Integer.parseInt(st.nextToken());
 				int y = Integer.parseInt(st.nextToken());
-				
+
 				arr[i] = new P(x, y);
 			}
-			
+
 			for (int i = 0; i < N + 2; i++) {
 				for (int j = 0; j < N + 2; j++) {
 					int xDiff = Math.abs(arr[i].x - arr[j].x);
 					int yDiff = Math.abs(arr[i].y - arr[j].y);
-					
+
 					dist[i][j] = xDiff + yDiff;
 				}
 			}
-			
-			pick(N);
-			
+
+			pick(N, 0, 0);
+
 			System.out.println("#" + tc + " " + min);
 		}
 	}
-	
-	public static void pick(int N) {
-		if (used.size() == N) {
-			int sum = dist[0][used.get(0)];
-			
-			for (int i = 0; i < N - 1; i++) {
-				sum += dist[used.get(i)][used.get(i + 1)];
-			}
-			
-			sum += dist[used.get(N - 1)][1];
-			
-			min = Math.min(min, sum);
+
+	public static void pick(int N, int depth, int sum) {
+		if (sum > min) return;
+		
+		if (depth == N) {
+			min = Math.min(min, sum + dist[pick[N - 1]][1]);
 		} else {
-			for (int i = 2; i < N + 2; i++) {
-				if (!used.contains(i)) {
-					used.add(i);
-					pick(N);
-					used.remove(used.size() - 1);
+			p: for (int i = 2; i < N + 2; i++) {
+				for (int j = 0; j < N; j++) {
+					if (pick[j] == i) {
+						continue p;
+					}
 				}
+				
+				int newSum = sum;
+				if (depth == 0) {
+					newSum += dist[0][i];
+				} else {
+					newSum += dist[pick[depth - 1]][i];
+				}
+				
+				pick[depth] = i;
+				pick(N, depth + 1, newSum);
+				pick[depth] = 0;
 			}
 		}
 	}
