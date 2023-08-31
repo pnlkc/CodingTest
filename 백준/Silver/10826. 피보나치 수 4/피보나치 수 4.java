baa99e929b05ba98f21ec13556fda4aa71b421ba
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-08-31 12:38:00 +0900

    [Silver V] Title: 피보나치 수 4, Time: 128 ms, Memory: 18052 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		BigInteger[] dp = new BigInteger[10_001];
+		dp[0] = BigInteger.ZERO;
+		dp[1] = BigInteger.ONE;
+		
+		for (int i = 2; i <= N; i++) {
+			dp[i] = dp[i - 1].add(dp[i - 2]);
+		}
+		
+		System.out.println(dp[N]);
+	}
+}
