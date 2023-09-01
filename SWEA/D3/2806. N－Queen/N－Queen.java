fa34889ca86956d0a1bfa59d6e16dc8dd971b0f6
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-09-01 10:18:15 +0900

    [D3] Title: N-Queen, Time: 129 ms, Memory: 21,164 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.java
+++ SWEA/D3/2806. N－Queen/N－Queen.java
@@ -5,7 +5,6 @@ import java.io.InputStreamReader;
 public class Solution {
 	static int N, cnt;
 	static int[] dx = { -1, 0, 1 };
-	static int[] dy = { 1, 1, 1 };
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,7 +16,7 @@ public class Solution {
 			cnt = 0;
 
 			calc(0, arr);
-			
+
 			System.out.println("#" + tc + " " + cnt);
 		}
 	}
@@ -25,29 +24,30 @@ public class Solution {
 	public static void calc(int i, int[][] arr) {
 		if (i == N) {
 			cnt++;
-		} else {
-			for (int j = 0; j < N; j++) {
-				if (arr[i][j] == 0) {
-					check(j, i, 1, arr);
-					calc(i + 1, arr);
-					check(j, i, -1, arr);
-				}
+			return;
+		}
+		
+		for (int j = 0; j < N; j++) {
+			if (arr[i][j] == 0) {
+				check(j, i, 1, arr);
+				calc(i + 1, arr);
+				check(j, i, -1, arr);
 			}
 		}
 	}
 
 	public static void check(int x, int y, int num, int[][] arr) {
 		arr[y][x] += num;
-		
+
 		for (int j = 1; j < N; j++) {
 			for (int d = 0; d < 3; d++) {
 				int nx = x + dx[d] * j;
-				int ny = y + dy[d] * j;
-				
+				int ny = y + j;
+
 				if (nx < 0 || ny < 0 || nx >= N || ny >= N) {
 					continue;
 				}
-				
+
 				arr[ny][nx] += num;
 			}
 		}
