c2e92740466f97cd51b7554864037fb14192df40
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-09-01 11:35:30 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 160 ms, Memory: 19,808 KB -BaekjoonHub

---
+++ SWEA/D4/1486. 장훈이의 높은 선반/README.md
@@ -0,0 +1,11 @@
+# [D4] 장훈이의 높은 선반 - 1486 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV2b7Yf6ABcBBASw) 
+
+### 성능 요약
+
+메모리: 19,808 KB, 시간: 160 ms, 코드길이: 1,209 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static int N, B, min;
+	static int[] arr;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st1 = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st1.nextToken());
+			B = Integer.parseInt(st1.nextToken());
+			arr = new int[N];
+			StringTokenizer st2 = new StringTokenizer(br.readLine());
+			min = Integer.MAX_VALUE;
+			
+			for (int i = 0; i < N; i++) {
+				arr[i] = Integer.parseInt(st2.nextToken());
+			}
+			
+			for (int i = 1; i <= N; i++) {
+				int[] pick = new int[i];
+				calc(pick, 0, 0, i, 0);
+			}
+			
+			System.out.println("#" + tc + " " + (min - B));
+		}
+	}
+	
+	public static void calc(int[] pick, int idx, int cnt, int target, int sum) {
+		if (cnt == target) {
+			if (B <= sum) {
+				min = Math.min(min, sum);
+			}
+
+			return;
+		}
+		
+		for (int i = idx; i < N; i++) {
+			pick[cnt] = i;
+			calc(pick, i + 1, cnt + 1, target, sum + arr[i]);
+		}
+	}
+}
