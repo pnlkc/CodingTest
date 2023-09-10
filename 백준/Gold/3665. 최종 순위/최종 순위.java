bdfac96db428a612e20e1312524791b71b1636ce
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-09-10 10:07:00 +0900

    [Gold I] Title: 최종 순위, Time: 904 ms, Memory: 91400 KB -BaekjoonHub

--- 백준/Gold/3665. 최종 순위/최종 순위.java
+++ 백준/Gold/3665. 최종 순위/최종 순위.java
@@ -21,6 +21,7 @@ public class Main {
 			int[] cnt = new int[n + 1];
 			List<Integer> list = new ArrayList<>();
 			
+			// graph 접근시 NullPointerException 방지
 			for (int i = 0; i <= n; i++) {
 				graph.add(new ArrayList<>());
 			}
@@ -31,7 +32,7 @@ public class Main {
 			for (int i = 0; i < n; i++) {
 				pastYear[i] = Integer.parseInt(st1.nextToken());
 				
-				if (i > 0) {
+				if (i > 0) { // 자신보다 높은 등수의 팀들과 간선 연결
 					for (int j = i - 1; j >= 0; j--) {
 						graph.get(pastYear[i]).add(pastYear[j]);
 						cnt[pastYear[j]]++;
@@ -46,12 +47,12 @@ public class Main {
 				int a = Integer.parseInt(st2.nextToken());
 				int b = Integer.parseInt(st2.nextToken());
 				
-				if (graph.get(b).contains(a)) {
+				if (graph.get(b).contains(a)) { // a가 더 높은 등수였던 경우
 					graph.get(b).remove((Integer) a);
 					cnt[a]--;
 					graph.get(a).add(b);
 					cnt[b]++;
-				} else {
+				} else { // b가 더 높은 등수였던 경우
 					graph.get(a).remove((Integer) b);
 					cnt[b]--;
 					graph.get(b).add(a);
@@ -61,12 +62,14 @@ public class Main {
 			
 			Queue<Integer> q = new LinkedList<>();
 			
+			// 진입차수가 0인 팀(노드)를 큐에 추가
 			for (int i = 1; i <= n; i++) {
 				if (cnt[i] == 0) {
 					q.add(i);
 				}
 			}
 			
+			// 큐가 빌 때까지 반복
 			while (!q.isEmpty()) {
 				int cn = q.poll();
 				list.add(cn);
@@ -78,23 +81,23 @@ public class Main {
 				}
 			}
 			
-			boolean isEnd = true;
+			boolean isEnd = true; // 등수를 알 수 있는지 확인하는 변수
 			
 			for (int i = 1; i <= n; i++) {
-				if (cnt[i] != 0) {
+				if (cnt[i] != 0) { // 데이터에 일관성이 없어서 순위를 정할 수 없는 경우
 					isEnd = false;
 					break;
 				}
 			}
 			
 			if (isEnd) {
-				if (list.size() == n) {
-					Collections.reverse(list);
+				if (list.size() == n) { // 모든 팀의 순위를 알 수 있는 경우
+					Collections.reverse(list); // 낮은 등 수 부터 역순으로 입력되었으므로 reverse 필요
 					System.out.println(list.stream().map(String::valueOf).collect(Collectors.joining(" ")));
-				} else {
+				} else { // 모든 팀의 확실한 순위를 찾을 수 없는 경우
 					System.out.println("?");
 				}
-			} else {
+			} else { // 데이터에 일관성이 없어서 순위를 정할 수 없는 경우
 				System.out.println("IMPOSSIBLE");
 			}
 		}
