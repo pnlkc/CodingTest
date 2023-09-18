ecae264889f9639e50ea2e17030732c0aa6c3636
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-09-18 11:08:10 +0900

    [Gold V] Title: 동전 2, Time: 260 ms, Memory: 18560 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// 백준 2294 동전 2
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n];
+		int[] dp = new int[k + 1];
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			if (arr[i] <= k) dp[arr[i]] = 1;
+		}
+
+		arr = Arrays.stream(arr).distinct().toArray();
+		Arrays.sort(arr);
+
+		if (arr[0] > k) {
+			System.out.println(-1);
+			return;
+		}
+		
+		for (int i = arr[0]; i <= k; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				if (arr[j] > i) break; 
+				
+				if (dp[i] == 0) {
+					if (i % arr[j] == 0 || dp[i % arr[j]] != 0) {
+						dp[i] = dp[i - arr[j]] + 1;
+					}
+				} else {
+					if (i % arr[j] == 0 || dp[i % arr[j]] != 0) {
+						dp[i] = Math.min(dp[i], dp[i - arr[j]] + 1);
+					}
+				}
+			}
+		}
+		
+		if (dp[k] == 0) {
+			System.out.println(-1);
+		} else {
+			System.out.println(dp[k]);
+		}
+	}
+}
