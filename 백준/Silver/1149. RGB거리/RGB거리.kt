7e2ed93b37165ebd9fec049aa22e3af492aa90ef
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-09-27 11:19:07 +0900

    [Silver I] Title: RGB거리, Time: 220 ms, Memory: 26504 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.kt
+++ 백준/Silver/1149. RGB거리/RGB거리.kt
@@ -1,12 +1,20 @@
 fun main() {
     var array = IntArray(3)
 
-    repeat(readln().toInt()) {
-        val input = readln().split(" ").map { it.toInt() }
-        val one = minOf(array[1] + input[0], array[2] + input[0])
-        val two = minOf(array[0] + input[1], array[2] + input[1])
-        val three = minOf(array[0] + input[2], array[1] + input[2])
-        array = intArrayOf(one, two, three)
+    repeat(readln().toInt()) { i ->
+        if (i == 0) {
+            array = readln().split(" ").map { it.toInt() }.toIntArray()
+        } else {
+            val input = readln().split(" ").map { it.toInt() }
+
+            val one = minOf(array[1] + input[0], array[2] + input[0])
+            val two = minOf(array[0] + input[1], array[2] + input[1])
+            val three = minOf(array[0] + input[2], array[1] + input[2])
+
+            array[0] = one
+            array[1] = two
+            array[2] = three
+        }
     }
 
     println(array.minOrNull()!!)
