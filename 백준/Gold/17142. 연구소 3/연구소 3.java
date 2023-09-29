5a1b87bb156dac553152fa467c454096af424e51
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-09-29 10:11:05 +0900

    Update 연구소 3.java

--- 백준/Gold/17142. 연구소 3/연구소 3.java
+++ 백준/Gold/17142. 연구소 3/연구소 3.java
@@ -8,10 +8,12 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M, result, maxCnt;
+	static int N, M;
+	static int result; // 최소 시간을 담을 변수
+	static int maxCnt; // 0의 개수를 담을 변수 (bfs시 모든 빈칸에 바이러스를 퍼트렸는지 확인용)
 	static int[] pick;
 	static int[][] map;
-	static List<Pos> vList = new ArrayList<>();
+	static List<Pos> vList = new ArrayList<>(); // 비활성 바이러스 리스트
 	static int[] dx = { 1, -1, 0, 0 };
 	static int[] dy = { 0, 0, 1, -1 };
 
@@ -24,16 +26,17 @@ public class Main {
 		pick = new int[M];
 		result = -1;
 
+		// 지도 초기화
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st2 = new StringTokenizer(br.readLine());
 
 			for (int j = 0; j < N; j++) {
 				map[i][j] = Integer.parseInt(st2.nextToken());
 
-				if (map[i][j] == 2) {
-					vList.add(new Pos(i, j));
-				} else if (map[i][j] == 0) {
-					maxCnt++;
+				if (map[i][j] == 2) { // 비활성 바이러스인 경우
+					vList.add(new Pos(i, j)); // 비활성 바이러스 리스트에 추가
+				} else if (map[i][j] == 0) { // 빈 칸인 경우
+					maxCnt++; // 빈 칸 개수 추가
 				}
 			}
 		}
@@ -43,35 +46,38 @@ public class Main {
 		System.out.println(result);
 	}
 
+	// 활성화 시킬 바이러스  선택하는 메소드
 	public static void pick(int idx, int cnt) {
-		if (cnt == M) {
-			int r = bfs(pick);
+		if (cnt == M) { // M개 만큼 고른 경우
+			int r = bfs(pick); // bfs 돌린 결과
 
-			if (r != -1 && result == -1) {
+			// bfs 돌린 결과 연구소의 모든 빈칸이 감염이 되었을 경우 최솟값 갱신
+			if (r != -1 && result == -1) { // 이전에 가능한 경우가 없었던 경우
 				result = r;
-			} else if (r != -1 && result != -1) {
+			} else if (r != -1 && result != -1) { // 이전에 가능한 경우가 있었던 경우
 				result = Math.min(result, r);
 			}
 
 			return;
 		}
 
-		if (idx >= vList.size()) {
+		if (idx >= vList.size()) { // M개를 선택하지 못한 경우 
 			return;
 		}
 
-		pick[cnt] = idx;
-		pick(idx + 1, cnt + 1);
-		pick(idx + 1, cnt);
+		pick[cnt] = idx; // cnt번에 idx를 선택
+		pick(idx + 1, cnt + 1); // 현재 idx 선택한 경우
+		pick(idx + 1, cnt); // 현재 idx 선택하지 않은 경우
 	}
 
+	// bfs 수행 메소드
 	public static int bfs(int[] sArr) {
-		int cnt = 0;
-		int r = 0;
+		int cnt = 0; // 연구소의 빈칸 감염 개수
+		int r = 0; // 진행 시간을 담을 변수
 		Queue<Pos> q = new LinkedList<>();
 		boolean[][] isVisit = new boolean[N][N];
-		int[][] newMap = new int[N][N];
 
+		// 시작 위치를 모두 큐에 추가 및 방문 처리
 		for (int i : sArr) {
 			Pos c = vList.get(i);
 			q.add(c);
@@ -80,43 +86,43 @@ public class Main {
 
 		while (!q.isEmpty()) {
 			Pos c = q.poll();
-			if (map[c.y][c.x] == 0) {
+			if (map[c.y][c.x] == 0) { // 빈 칸인 경우 진행 시간 갱신
 				r = Math.max(r, c.d);
 			}
+			
+			if (result != -1 && r >= result) { // 진행 상황이 최솟값보다 크면 탐색 중지
+				return -1;
+			}
 
-			for (int d = 0; d < 4; d++) {
+			// 4방 탐색
+			for (int d = 0; d < 4; d++) { 
 				int nx = c.x + dx[d];
 				int ny = c.y + dy[d];
 
-				if (nx < 0 || ny < 0 || nx >= N || ny >= N) {
+				if (nx < 0 || ny < 0 || nx >= N || ny >= N) { // 범위를 벗어난 경우
 					continue;
 				}
 
-				if (map[ny][nx] == 1) {
+				if (map[ny][nx] == 1) { // 벽인 경우
 					continue;
 				}
 
-				if (isVisit[ny][nx]) {
+				if (isVisit[ny][nx]) { // 이미 방문한 경우
 					continue;
 				}
 
-				if (map[ny][nx] == 0) {
-					cnt++;
-				}
-				
-				if (result != -1 && r >= result) {
-					return -1;
+				if (map[ny][nx] == 0) { // 빈 칸인 경우
+					cnt++; // cnt 추가
 				}
 				
 				isVisit[ny][nx] = true;
 				q.add(new Pos(ny, nx, c.d + 1));
-				newMap[ny][nx] = c.d + 1;
 			}
 		}
 
-		if (cnt == maxCnt) {
+		if (cnt == maxCnt) { // 연구소의 모든 빈칸이 감염된 경우
 			return r;
-		} else {
+		} else { // 연구소의 모든 빈칸이 감염되지 않은 경우
 			return -1;
 		}
 	}
