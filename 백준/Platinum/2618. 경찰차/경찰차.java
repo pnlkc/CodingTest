e427ec70d7efbecd7b2110a523fde997ea92ecc6
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-10-05 11:12:51 +0900

    [Platinum IV] Title: 경찰차, Time: 532 ms, Memory: 71404 KB -BaekjoonHub

--- 백준/Platinum/2618. 경찰차/경찰차.java
+++ 백준/Platinum/2618. 경찰차/경찰차.java
@@ -1,7 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 class Pos {
