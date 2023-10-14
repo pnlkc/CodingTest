4e01cc687e56f4ba17314beb3c8dddcc8bb4f145
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-10-14 09:44:29 +0900

    [Gold III] Title: 과제, Time: 288 ms, Memory: 19624 KB -BaekjoonHub

---
+++ 백준/Gold/13904. 과제/과제.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Data> pq = new PriorityQueue<>(Comparator.comparing(Data::getD).thenComparing(Data::getW, Comparator.reverseOrder()));
+
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int d = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+
+			pq.add(new Data(d, w));
+		}
+
+		int[] result = new int[1_001];
+		
+		while (!pq.isEmpty()) {
+			Data c = pq.poll();
+			int min = Integer.MAX_VALUE;
+			int minIdx = -1;
+			
+			for (int i = 1; i <= c.d; i++) {
+				if (result[i] < c.w && result[i] < min) {
+					min = result[i];
+					minIdx = i;
+				}
+			}
+			
+			if (minIdx != -1) {
+				result[minIdx] = c.w;
+			}
+		}
+		
+		int sum = 0;
+		for (int i = 1; i <= 1_000; i++) {
+			sum += result[i];
+		}
+		
+		System.out.println(sum);
+	}
+}
+
+class Data {
+	int d, w;
+
+	public Data(int d, int w) {
+		this.d = d;
+		this.w = w;
+	}
+
+	public int getD() {
+		return d;
+	}
+
+	public int getW() {
+		return w;
+	}
+}
