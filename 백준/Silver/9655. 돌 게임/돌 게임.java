966703c00f6525095beedfe6396463ec85d7bc44
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-10-16 17:28:02 +0900

    [Silver V] Title: 돌 게임, Time: 76 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -6,17 +6,13 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int[] dp = new int[N + 1];
+		int[] dp = new int[Math.max(4, N + 1)];
+		dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 1;
 		
-		
-		for (int i = 1; i <= N; i++) {
-			if (i == 1) {
-				dp[1] = 1;
-			} else if (i == 2) {
-				dp[2] = 2;
-			} else {
-				dp[i] = Math.min(dp[i - 1], dp[i - 3]) + 1;
-			}
+		for (int i = 4; i <= N; i++) {
+			dp[i] = Math.min(dp[i - 1], dp[i - 3]) + 1;
 		}
 		
 		if (dp[N] % 2 == 0) {
