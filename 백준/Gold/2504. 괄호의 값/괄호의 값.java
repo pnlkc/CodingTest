3d260b7453d3f88388be956f0c1ec91752ad0e18
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-10-22 10:53:20 +0900

    [Gold V] Title: 괄호의 값, Time: 76 ms, Memory: 11548 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.java
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 class Main {
@@ -8,6 +10,11 @@ class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str = br.readLine();
 		Stack<String> s = new Stack<>();
+		Map<String, String> map = new HashMap<>();
+		map.put(")", "(");
+		map.put("(", "2");
+		map.put("]", "[");
+		map.put("[", "3");
 
 		for (int i = 0; i < str.length(); i++) {
 			String c = String.valueOf(str.charAt(i));
@@ -15,60 +22,38 @@ class Main {
 			if (c.equals("(") || c.equals("[")) { // 여는 괄호인 경우
 				s.push(c);
 			} else { // 닫는 괄호인 경우
-				if (c.equals(")")) { // 소괄호 인 경우
-					if (!s.isEmpty() && s.peek().equals("(")) { // 소괄호 짝이 맞는 경우
-						s.pop();
-						s.push("2");
-					} else if (s.isEmpty() || s.peek().equals("[")) { // 짝이 맞지 않는 경우
+				if (!s.isEmpty() && s.peek().equals(map.get(c))) { // 소괄호 짝이 맞는 경우
+					s.pop();
+					s.push(map.get(map.get(c)));
+				} else if (s.isEmpty() || map.get(s.peek()) != null) { // 짝이 맞지 않는 경우
+					System.out.println(0);
+					return;
+				} else { // 앞에 숫자가 있는 경우
+					int num = 0;
+
+					// 앞에 숫자가 없을 때까지 반복
+					while (!s.isEmpty() && Character.isDigit(s.peek().charAt(0))) {
+						num += Integer.parseInt(s.pop());
+					}
+
+					if (s.isEmpty() || (!map.get(c).equals(s.peek()) && map.get(s.peek()) != null)) { // 올바르지 못한 괄호열인 경우
 						System.out.println(0);
 						return;
-					} else {
-						int num = 0;
-
-						while (!s.isEmpty() && Character.isDigit(s.peek().charAt(0))) {
-							num += Integer.parseInt(s.pop());
-						}
-
-						if (s.isEmpty() || s.peek().equals("[")) {
-							System.out.println(0);
-							return;
-						}
-
-						s.pop();
-						s.push(String.valueOf(num * 2));
 					}
-				} else { // 대괄호인 경우
-					if (!s.isEmpty() && s.peek().equals("[")) { // 대괄호 짝이 맞는 경우
-						s.pop();
-						s.push("3");
-					} else if (s.isEmpty() || s.peek().equals("(")) { // 짝이 맞지 않는 경우
-						System.out.println(0);
-						return;
-					} else {
-						int num = 0;
 
-						while (!s.isEmpty() && Character.isDigit(s.peek().charAt(0))) {
-							num += Integer.parseInt(s.pop());
-						}
-
-						if (s.isEmpty() || s.peek().equals("(")) {
-							System.out.println(0);
-							return;
-						}
-
-						s.pop();
-						s.push(String.valueOf(num * 3));
-					}
+					s.pop();
+					s.push(String.valueOf(num * Integer.parseInt(map.get(map.get(c)))));
 				}
 			}
 		}
 
 		int result = 0;
 		
+		// stack에 남은 요소들 계산
 		while (!s.isEmpty()) {
 			if (Character.isDigit(s.peek().charAt(0))) {
 				result += Integer.parseInt(s.pop());
-			} else {
+			} else { // 올바르지 못한 괄호열인 경우
 				System.out.println(0);
 				return;
 			}
