2b0eaca406738c9de1075c3f053e80d5ec367081
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-11-01 11:12:43 +0900

    [Gold V] Title: ABCDE, Time: 2056 ms, Memory: 293864 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	static List<List<Integer>> graph;
+	static boolean isPossible = false;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		graph = new ArrayList<>();
+		
+		for (int i = 0; i < N; i++) {
+			graph.add(new ArrayList<>());
+		}
+		
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			graph.get(a).add(b);
+			graph.get(b).add(a);
+		}
+		
+		for (int i = 0; i < N; i++) {
+			boolean[] isVisit = new boolean[N];
+			isVisit[i] = true;
+			dfs(i, 1, isVisit);
+		}
+		
+		if (isPossible) {
+			System.out.println(1);
+		} else {
+			System.out.println(0);
+		}
+	}
+	
+	public static void dfs(int n, int depth, boolean[] isVisit) {
+		if (isPossible) {
+			return;
+		}
+		
+		if (depth == 5) {
+			isPossible = true;
+			return;
+		}
+		
+		for (int next : graph.get(n)) {
+			if (!isVisit[next]) {
+				boolean[] temp = isVisit.clone();
+				temp[next] = true;
+				dfs(next, depth + 1, temp);
+			}
+		}
+	}
+}
