5ff060149002ac52dac95984d9a788ca12484595
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-11-04 12:18:47 +0900

    [Gold IV] Title: 전화번호 목록, Time: 524 ms, Memory: 189588 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -1,14 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
-import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
@@ -16,25 +10,27 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int t = Integer.parseInt(br.readLine());
 		
-		p: for (; t > 0; t--) {
+		for (; t > 0; t--) {
 			int n = Integer.parseInt(br.readLine());
-			String[] arr = new String[n];
+			Trie trie = new Trie();
+			boolean isNo = false;
 			
 			for (int i = 0; i < n; i++) {
-				arr[i] = br.readLine();
-			}
-			
-			Arrays.sort(arr, (String s1, String s2) -> s1.length() - s2.length());
-		
-			Trie trie = new Trie();
-			
-			for (String str : arr) {
-				if (!trie.check(str)) {
+				if (isNo) {
+					br.readLine();
+					continue;
+				}
+				
+				if (!trie.check(br.readLine())) {
 					sb.append("NO\n");
-					continue p;
+					isNo = true;
 				}
 			}
 			
+			if (isNo) {
+				continue;
+			}
+	
 			sb.append("YES\n");
 		}
 		
@@ -50,10 +46,16 @@ class Trie {
 		
 		for (int i = 0; i < str.length(); i++) {
 			char c = str.charAt(i);
+						
 			node = node.children.computeIfAbsent(c, key -> new Node());
+	
 			if (node.isEnd) {
 				return false;
 			}
+		}
+		
+		if (!node.children.isEmpty()) {
+			return false;
 		}
 		
 		node.isEnd = true;
