9806d911949ac75fa60f25dbdd3f02132fded3cf
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-12-05 08:58:20 +0900

    [Gold V] Title: 선 긋기, Time: 1736 ms, Memory: 325900 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.java
+++ 백준/Gold/2170. 선 긋기/선 긋기.java
@@ -1,15 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		Pos[] arr = new Pos[N];
+		PriorityQueue<Pos> pq = new PriorityQueue<>((Pos p1, Pos p2) -> p1.s - p2.s);
 		int sum = 0;
 		int S = Integer.MIN_VALUE;
 		int E = Integer.MIN_VALUE;
@@ -19,13 +18,11 @@ public class Main {
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
 			
-			arr[i] = new Pos(x, y);
+			pq.offer(new Pos(x, y));
 		}
 		
-		Arrays.sort(arr, Comparator.comparing(Pos::getS));
-
 		for (int i = 0; i < N; i++) {
-			Pos c = arr[i];
+			Pos c = pq.poll();
 			
 			if (c.s <= E) {
 				if (c.e > E) {
@@ -50,9 +47,5 @@ class Pos {
 	public Pos(int s, int e) {
 		this.s = s;
 		this.e = e;
-	}
-
-	public int getS() {
-		return s;
 	}
 }
