62927d25ee6a30c849241bdeb222f7fe4adc1cc2
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2023-12-10 10:22:25 +0900

    [Gold IV] Title: 작업, Time: 3248 ms, Memory: 321456 KB -BaekjoonHub

---
+++ 백준/Gold/2056. 작업/작업.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		List<List<Integer>> graph = new ArrayList<>();
+		int[] times = new int[N + 1];
+		int[] cnt = new int[N + 1];
+		
+		for (int i = 0; i <= N; i++) {
+			graph.add(new ArrayList<>());
+		}
+		
+		for (int i = 1; i <= N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int time = Integer.parseInt(st.nextToken());
+			int num = Integer.parseInt(st.nextToken());
+			
+			times[i] = time;
+			
+			for (int j = 0; j < num; j++) {
+				int next = Integer.parseInt(st.nextToken());
+				graph.get(next).add(i);
+				cnt[i]++;
+			}
+		}
+		
+		Queue<Data> q = new LinkedList<>();
+		int[] isVisit = new int[N + 1];
+		
+		for (int i = 1; i <= N; i++) {
+			if (cnt[i] == 0) {
+				q.offer(new Data(i, times[i]));
+				isVisit[i] = times[i];
+			}
+		}
+		
+		int result = 0;
+		
+		while (!q.isEmpty()) {
+			Data c = q.poll();
+			
+			if (isVisit[c.num] > c.time) {
+				continue;
+			}
+			
+			result = Math.max(result, c.time);
+			
+			for (int next : graph.get(c.num)) {
+				if (isVisit[next] < c.time + times[next]) {
+					q.offer(new Data(next, c.time + times[next]));
+					isVisit[next] = c.time + times[next];
+				}
+			}
+		}
+		
+		System.out.println(result);
+	}
+}
+
+class Data {
+	int num, time;
+
+	public Data(int num, int time) {
+		this.num = num;
+		this.time = time;
+	}
+}
