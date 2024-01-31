88e5d5f93fff8ef38114e7c167af457c4f8a2522
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2024-01-31 20:31:42 +0900

    [Silver IV] Title: 국영수, Time: 840 ms, Memory: 85464 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.kt
+++ 백준/Silver/10825. 국영수/국영수.kt
@@ -1,25 +1,27 @@
+import java.util.PriorityQueue
+
 data class Data(val name: String, val a: Int, val b: Int, val c: Int)
 
 fun main() {
     val N = readln().toInt()
     val list = mutableListOf<Data>()
     val sb = StringBuilder()
+    val pq = PriorityQueue(
+        compareByDescending<Data> { it.a }
+            .thenBy { it.b }
+            .thenByDescending { it.c }
+            .thenBy { it.name }
+    )
 
     for (i in 1..N) {
         val (name, a, b, c) = readln().split(" ")
 
-        list.add(Data(name, a.toInt(), b.toInt(), c.toInt()))
+        pq.offer(Data(name, a.toInt(), b.toInt(), c.toInt()))
+    }
+    
+    while (pq.isNotEmpty()) {
+        sb.append("${pq.poll()!!.name}\n")
     }
 
-    list
-        .sortWith(
-            compareByDescending<Data> { it.a }
-                .thenBy { it.b }
-                .thenByDescending { it.c }
-                .thenBy { it.name }
-        )
-
-    list.forEach { sb.append("${it.name}\n") }
-    
     println(sb)
 }
