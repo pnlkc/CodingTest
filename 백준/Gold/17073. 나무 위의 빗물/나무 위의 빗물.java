e3665fe68c79815a7acc8057caf3bb8fd92cd5c8
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2024-05-18 09:53:53 +0900

    [Gold V] Title: 나무 위의 빗물, Time: 496 ms, Memory: 131504 KB -BaekjoonHub

--- 백준/Gold/17073. 나무 위의 빗물/나무 위의 빗물.java
+++ 백준/Gold/17073. 나무 위의 빗물/나무 위의 빗물.java
@@ -1,10 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 class Main {
@@ -13,41 +9,20 @@ class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		double W = Double.parseDouble(st.nextToken());
-		List<List<Integer>> graph = new ArrayList<>();
+		int[] nodeNum = new int[N + 1];
 		int cnt = 0;
-		
-		for (int i = 0; i <= N; i++) {
-			graph.add(new ArrayList<>());
-		}
 		
 		for (int i = 0; i < N - 1; i++) {
 			st = new StringTokenizer(br.readLine());
 			int U = Integer.parseInt(st.nextToken());
 			int V = Integer.parseInt(st.nextToken());
 			
-			graph.get(U).add(V);
-			graph.get(V).add(U);
+			nodeNum[U]++;
+			nodeNum[V]++;
 		}
 		
-		Queue<Integer> q = new LinkedList<>();
-		boolean[] isVisit = new boolean[N + 1];
-		
-		q.add(1);
-		isVisit[1] = true;
-		
-		while (!q.isEmpty()) {
-			int c = q.poll();
-			int num = 0;
-			
-			for (int next : graph.get(c)) {
-				if (isVisit[next]) continue;
-				
-				num++;
-				q.add(next);
-				isVisit[next] = true;
-			}
-			
-			if (num == 0) cnt++;
+		for (int i = 2; i <= N; i++) {
+			if (nodeNum[i] == 1) cnt++;
 		}
 		
 		System.out.println(String.format("%.10f", W / cnt));
