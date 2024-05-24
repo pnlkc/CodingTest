98745545dd28a768c23e01c6bf89abc1007bca1e
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2024-05-24 09:23:18 +0900

    [Gold V] Title: 카드게임, Time: 564 ms, Memory: 165664 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10835. 카드게임/카드게임.kt
+++ 백준/Gold/10835. 카드게임/카드게임.kt
@@ -2,30 +2,24 @@ fun main() {
     val N = readln().toInt()
     val lL = readln().split(" ").map { it.toInt() }
     val rL = readln().split(" ").map { it.toInt() }
-    val dp = Array(N + 1) { IntArray(N + 1) }
-    var max = 0
+    val dp = Array(N + 1) { IntArray(N + 1) { -1 } }
 
-    calc(0, 0, lL, rL, dp, 0)
+    calc(0, 0, lL, rL, dp)
 
-    for (i in 0 until N) {
-        max = maxOf(max, dp[i][N])
-        max = maxOf(max, dp[N][i])
-    }
-
-    println(max)
+    println(dp[0][0])
 }
 
-fun calc(l: Int, r: Int, lL: List<Int>, rL: List<Int>, dp: Array<IntArray>, result: Int) {
-    if (l >= lL.size || r >= rL.size) return
-    if (dp[l][r] > result) return
+fun calc(l: Int, r: Int, lL: List<Int>, rL: List<Int>, dp: Array<IntArray>): Int {
+    if (l >= lL.size || r >= rL.size) return 0
 
-    dp[l + 1][r] = maxOf(dp[l + 1][r], result)
-    calc(l + 1, r, lL, rL, dp, result)
-    dp[l + 1][r + 1] = maxOf(dp[l + 1][r + 1], result)
-    calc(l + 1, r + 1, lL, rL, dp, result)
+    if (dp[l][r] != -1) return dp[l][r]
 
-    if (lL[l] > rL[r]) {
-        dp[l][r + 1] = maxOf(dp[l][r + 1], result + rL[r])
-        calc(l, r + 1, lL, rL, dp, result + rL[r])
-    }
+    dp[l][r] = maxOf(
+        0,
+        calc(l + 1, r, lL, rL, dp),
+        calc(l + 1, r + 1, lL, rL, dp),
+        if (lL[l] > rL[r]) rL[r] + calc(l, r + 1, lL, rL, dp) else 0
+    )
+
+    return dp[l][r]
 }
