bb76939f7102cbc7bcc74e4d87e6375feeeda8a0
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2024-05-24 10:46:49 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.11 ms, Memory: 62 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.kt
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.kt
@@ -1,41 +1,35 @@
 class Solution {
     fun solution(n: Int): Array<IntArray> {
         val answer =  Array(n) { IntArray(n) }
+        val dx = arrayOf(1, 0, -1, 0)
+        val dy = arrayOf(0, 1, 0, -1)
         var x = 0
         var y = 0
         var num = 1
         var c = n
 
         while (num <= n * n) {
-            repeat(c) {
-                answer[y][x++] = num++
+            for (d in 0 until 4) {
+                for (i in 0 until c) {
+                    answer[y + dy[d] * i][x + dx[d] * i] = num++
+                }
+
+                y += dy[d] * (c - 1)
+                x += dx[d] * (c - 1)
+
+                when (d) {
+                    0 -> {
+                        y++
+                        c--
+                    }
+                    1 -> x--
+                    2 -> {
+                        y--
+                        c--
+                    }
+                    3 -> x++
+                }
             }
-
-            c--
-            x--
-            y++
-
-            repeat(c) {
-                answer[y++][x] = num++
-            }
-
-            y--
-            x--
-
-            repeat(c) {
-                answer[y][x--] = num++
-            }
-
-            x++
-            y--
-            c--
-
-            repeat(c) {
-                answer[y--][x] = num++
-            }
-
-            y++
-            x++
         }
 
         return answer
