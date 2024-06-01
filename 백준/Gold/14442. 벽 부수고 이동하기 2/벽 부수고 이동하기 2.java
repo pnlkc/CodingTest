9b12f91f82f12d5a01e980b2838299fc4cae9913
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2024-06-01 10:56:00 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1604 ms, Memory: 375264 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -15,7 +15,7 @@ class Main {
 		int M = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
 		int[][] map = new int[N][M];
-		int[][][] isVisit = new int[N][M][K + 1];
+		boolean[][][] isVisit = new boolean[N][M][K + 1];
 
 		for (int i = 0; i < N; i++) {
 			String input = br.readLine();
@@ -25,45 +25,40 @@ class Main {
 			}
 		}
 
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				for (int k = 0; k <= K; k++) {
-					isVisit[i][j][k] = Integer.MAX_VALUE;
-				}
-			}
-		}
-
 		Queue<Pos> q = new LinkedList<>();
 
 		q.offer(new Pos(0, 0, 1, 0));
-		isVisit[0][0][0] = 1;
+		isVisit[0][0][0] = true;
 
 		while (!q.isEmpty()) {
 			Pos c = q.poll();
-			
+
 			if (c.x == M - 1 && c.y == N - 1) {
 				System.out.println(c.time);
 				return;
 			}
-			if (isVisit[c.y][c.x][c.cnt] < c.time) continue;
-			
+
 			for (int d = 0; d < 4; d++) {
 				int nx = c.x + dx[d];
 				int ny = c.y + dy[d];
-				
-				if (nx < 0 || ny < 0 || nx >= M || ny >= N) continue;
-				
+
+				if (nx < 0 || ny < 0 || nx >= M || ny >= N)
+					continue;
+
 				if (map[ny][nx] == 0) {
-					if (isVisit[ny][nx][c.cnt] <= c.time + 1) continue;
-					
+					if (isVisit[ny][nx][c.cnt])
+						continue;
+
 					q.offer(new Pos(nx, ny, c.time + 1, c.cnt));
-					isVisit[ny][nx][c.cnt] = c.time + 1;
+					isVisit[ny][nx][c.cnt] = true;
 				} else {
-					if (c.cnt + 1 > K) continue;
-					if (isVisit[ny][nx][c.cnt + 1] <= c.time + 1) continue;
-					
+					if (c.cnt + 1 > K)
+						continue;
+					if (isVisit[ny][nx][c.cnt + 1])
+						continue;
+
 					q.offer(new Pos(nx, ny, c.time + 1, c.cnt + 1));
-					isVisit[ny][nx][c.cnt + 1] = c.time + 1;
+					isVisit[ny][nx][c.cnt + 1] = true;
 				}
 			}
 
