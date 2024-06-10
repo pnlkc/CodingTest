a53ce31951f25c1351a1acc4ddd2df09bb11aa41
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2024-06-10 09:31:57 +0900

    [Silver IV] Title: 복권, Time: 164 ms, Memory: 21936 KB -BaekjoonHub

--- 백준/Silver/1359. 복권/복권.kt
+++ 백준/Silver/1359. 복권/복권.kt
@@ -2,13 +2,11 @@ var cnt = 0
 
 fun main() {
     val (N, M, K) = readln().split(" ").map { it.toInt() }
-
-    pick(N, M, K, 1, mutableListOf())
-
     var total = 1
+    
     (N downTo N - M + 1).forEach { total *= it }
     (1..M).forEach { total /= it }
-
+    pick(N, M, K, 1, mutableListOf())
     println(cnt.toDouble() / total)
 }
 
@@ -18,8 +16,6 @@ fun pick(n: Int, m: Int, k: Int, num: Int, pick: MutableList<Int>) {
         return
     }
 
-    val temp = pick.toMutableList()
-    temp.add(num)
-    pick(n, m, k, num + 1, temp)
+    pick(n, m, k, num + 1, pick.toMutableList().also { it.add(num) })
     pick(n, m, k, num + 1, pick)
 }
