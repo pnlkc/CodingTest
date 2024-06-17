eef0d55593fbff2f0fb58cbb020e78982ec484d5
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2024-06-17 09:19:06 +0900

    [Gold V] Title: 벽장문의 이동, Time: 144 ms, Memory: 23784 KB -BaekjoonHub

--- 백준/Gold/2666. 벽장문의 이동/벽장문의 이동.kt
+++ 백준/Gold/2666. 벽장문의 이동/벽장문의 이동.kt
@@ -1,29 +1,30 @@
 import kotlin.math.abs
 
+val list = mutableListOf<Int>()
+var dp = arrayOf<Array<IntArray>>()
+
 fun main() {
     val n = readln().toInt()
     val (o1, o2) = readln().split(" ").map { it.toInt() }.sorted()
     val m = readln().toInt()
-    val list = mutableListOf<Int>().apply { repeat(m) { add(readln().toInt()) } }
-    val dp = Array(m) { Array(n + 1) { IntArray(n + 1) } }
+    repeat(m) { list.add(readln().toInt()) }
+    dp = Array(m) { Array(n + 1) { IntArray(n + 1) } }
 
-    print(calc(0, o1, o2, list, dp))
+    print(calc(0, o1, o2))
 }
 
-fun calc(idx: Int, o1: Int, o2: Int, list: MutableList<Int>, dp: Array<Array<IntArray>>): Int {
-    if (idx == list.size) {
-        return 0
-    }
+fun calc(idx: Int, o1: Int, o2: Int): Int {
+    if (idx == list.size) return 0
 
     if (dp[idx][o1][o2] == 0) {
         val num = list[idx]
         dp[idx][o1][o2] = when {
-            list[idx] <= o1 -> abs(o1 - num) + calc(idx + 1, num, o2, list, dp)
-            list[idx] >= o2 -> abs(o2 - num) + calc(idx + 1, o1, num, list, dp)
+            list[idx] <= o1 -> abs(o1 - num) + calc(idx + 1, num, o2)
+            list[idx] >= o2 -> abs(o2 - num) + calc(idx + 1, o1, num)
             else -> {
                 minOf(
-                    abs(o1 - num) + calc(idx + 1, num, o2, list, dp),
-                    abs(o2 - num) + calc(idx + 1, o1, num, list, dp)
+                    abs(o1 - num) + calc(idx + 1, num, o2),
+                    abs(o2 - num) + calc(idx + 1, o1, num)
                 )
             }
         }
