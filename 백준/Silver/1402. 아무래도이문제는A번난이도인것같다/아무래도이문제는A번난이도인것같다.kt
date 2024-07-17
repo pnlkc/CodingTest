d66902be65b3a079babfe01e4e1a65f74d74890c
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2024-07-18 08:42:11 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 136 ms, Memory: 20756 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val sb = StringBuilder()
+    val T = readln().toInt()
+
+    for (tc in 1..T) {
+        val (A, B) = readln().split(" ").map { it.toInt() }
+
+        sb.appendLine("yes")
+    }
+    
+    print(sb)
+}
