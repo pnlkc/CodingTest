68d4569c181afb5a7a14349f863044b90bcea7f4
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2024-08-15 10:53:36 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 116 ms, Memory: 20712 KB -BaekjoonHub

---
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.kt
@@ -0,0 +1,28 @@
+var N = 0
+val sb = StringBuilder()
+val prefix = "____"
+
+fun main() {
+    N = readln().toInt()
+    sb.appendLine("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.")
+    recursion(0)
+    print(sb)
+}
+
+fun recursion(n: Int) {
+    if (n == N) {
+        sb.appendLine(makeString("\"재귀함수가 뭔가요?\"", n))
+        sb.appendLine(makeString("\"재귀함수는 자기 자신을 호출하는 함수라네\"", n))
+        sb.appendLine(makeString("라고 답변하였지.", n))
+        return
+    }
+
+    sb.appendLine(makeString("\"재귀함수가 뭔가요?\"", n))
+    sb.appendLine(makeString("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.", n))
+    sb.appendLine(makeString("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.", n))
+    sb.appendLine(makeString("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"", n))
+    recursion(n + 1)
+    sb.appendLine(makeString("라고 답변하였지.", n))
+}
+
+fun makeString(str: String, n: Int) = prefix.repeat(n) + str
