f567053356011c420bb5a1374df57b305f0025f3
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2024-10-04 14:29:55 +0900

    [Silver III] Title: 선물, Time: 180 ms, Memory: 24056 KB -BaekjoonHub

--- 백준/Silver/5911. 선물/선물.kt
+++ 백준/Silver/5911. 선물/선물.kt
@@ -10,21 +10,26 @@ fun main() {
         arr[i] = Data(p, s)
     }
 
+    arr.sortBy { it.p + it.s }
+
     for (i in 0 until N) {
         val ori = arr[i]
         arr[i] = Data(arr[i].p / 2, arr[i].s)
-        val temp =  arr.sortedBy { it.p + it.s }
 
         var sum = 0L
         var cnt = 0
 
         while (cnt < N) {
-            if (sum + temp[cnt].p + temp[cnt].s <= B) {
-                sum += temp[cnt].p.toLong() + temp[cnt].s
+            if (sum + arr[cnt].p + arr[cnt].s <= B) {
+                sum += arr[cnt].p.toLong() + arr[cnt].s
                 cnt++
             } else {
                 break
             }
+        }
+        
+        if (cnt < i && sum + ori.p / 2 + ori.s <= B) {
+            cnt++
         }
 
         result = maxOf(result, cnt)
