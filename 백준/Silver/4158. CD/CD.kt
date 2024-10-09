eeada8bc18e69b476737c04a068d5cb7d3ac0453
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2024-10-09 10:42:11 +0900

    [Silver V] Title: CD, Time: 1116 ms, Memory: 352472 KB -BaekjoonHub

--- 백준/Silver/4158. CD/CD.kt
+++ 백준/Silver/4158. CD/CD.kt
@@ -1,22 +1,30 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
+
 fun main() {
     val sb = StringBuilder()
     val map = mutableMapOf<Int, Boolean>()
+    val br = BufferedReader(InputStreamReader(System.`in`))
 
     while (true) {
-        val (N, M) = readln().split(" ").map { it.toInt() }
+        val st = StringTokenizer(br.readLine())
+        val N = st.nextToken().toInt()
+        val M = st.nextToken().toInt()
         if (N == 0 && M == 0) break
         map.clear()
         var cnt = 0
         var last = 0
 
         for (i in 0 until N) {
-            val num = readln().toInt()
+            val num = br.readLine().toInt()
             if (i == N - 1) last = num
             map[num] = true
         }
 
         for (i in 0 until M) {
-            val num = readln().toInt()
+            val num = br.readLine().toInt()
             if (num > last) continue
             if (map[num] != null) cnt++
         }
