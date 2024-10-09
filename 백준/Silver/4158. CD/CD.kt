0d6f47af95cdb49b04572e97af7ddf024de0ba95
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2024-10-09 10:37:30 +0900

    [Silver V] Title: CD, Time: 1696 ms, Memory: 357112 KB -BaekjoonHub

--- 백준/Silver/4158. CD/CD.kt
+++ 백준/Silver/4158. CD/CD.kt
@@ -7,14 +7,17 @@ fun main() {
         if (N == 0 && M == 0) break
         map.clear()
         var cnt = 0
+        var last = 0
 
         for (i in 0 until N) {
             val num = readln().toInt()
+            if (i == N - 1) last = num
             map[num] = true
         }
 
         for (i in 0 until M) {
             val num = readln().toInt()
+            if (num > last) continue
             if (map[num] != null) cnt++
         }
 
