bc1bebbfc3a6e9cd86bc4b526461079115351faa
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2024-10-12 10:39:45 +0900

    [Gold IV] Title: 세 수의 합, Time: 336 ms, Memory: 66612 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.kt
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.kt
@@ -1,14 +1,14 @@
 fun main() {
     val N = readln().toInt()
     val arr = IntArray(N)
-    val map = mutableMapOf<Int, Boolean>()
+    val map = mutableMapOf<Int, Byte>()
     var r = 0
 
     for (i in 0 until N) {
         arr[i] = readln().toInt()
 
         for (j in i downTo 0) {
-            map[arr[i] + arr[j]] = true
+            map[arr[i] + arr[j]] = 0
         }
     }
 
