1ee5697f0bd1f91b94cb83e578fbf43a83ca9fda
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2024-12-17 11:34:04 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 72 ms, Memory: 12308 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val input = readln()
+
+    println(
+        when (input) {
+            "SONGDO" -> "HIGHSCHOOL"
+            "CODE" -> "MASTER"
+            "2023" -> "0611"
+            else -> "CONTEST"
+        }
+    )
+}
