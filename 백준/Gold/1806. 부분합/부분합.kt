0c61c07ac172746c619ff9a627e90a01c0d40367
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2024-12-29 10:24:32 +0900

    [Gold IV] Title: 부분합, Time: 320 ms, Memory: 33492 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.kt
+++ 백준/Gold/1806. 부분합/부분합.kt
@@ -5,26 +5,15 @@ fun main() {
     val st = StringTokenizer(readln())
     val list = List(n) { st.nextToken().toInt() }
     var min = Int.MAX_VALUE
-    var sum = list[0]
+    var sum = 0
     var l = 0
     var r = 0
 
-    while (l < n || r < n) {
-        if (sum < s) {
-            if (r < n) {
-                if (++r != n) sum += list[r]
-            } else {
-                sum -= list[l++]
-            }
-        } else {
-            if (min > r - l + 1) min = r - l + 1
-            if (l < n) {
-                sum -= list[l++]
-            } else {
-                if (++r != n) sum += list[r]
-            }
+    while (l < n) {
+        if (sum < s && r < n) sum += list[r++]
+        else sum -= list[l++]
 
-        }
+        if (sum >= s) min = minOf(min, r - l)
     }
 
     println(if (min == Int.MAX_VALUE) 0 else min)
