3b27f94705e51bda94408c4ec16f8b0d22149000
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2025-01-10 13:21:19 +0900

    [Gold IV] Title: 고냥이, Time: 140 ms, Memory: 15472 KB -BaekjoonHub

--- 백준/Gold/16472. 고냥이/고냥이.kt
+++ 백준/Gold/16472. 고냥이/고냥이.kt
@@ -1,34 +1,32 @@
 fun main() {
     val N = readln().toInt()
     val input = readln()
-    val map = mutableMapOf<Char, Int>()
+    val map = IntArray(26)
     var cnt = 0
     var result = 0
     var max = 0
-
-    ('a'..'z').forEach { map[it] = 0 }
 
     var s = 0
     var e = 0
 
     while (e in s until input.length) {
-        val c = input[e]
+        val c = input[e] - 'a'
 
         if (map[c] == 0) {
             if (cnt >= N) {
-                map[input[s]] = map[input[s]]!! - 1
+                map[input[s] - 'a'] = map[input[s] - 'a'] - 1
                 result--
-                if (map[input[s]] == 0) cnt--
+                if (map[input[s] - 'a'] == 0) cnt--
                 s++
             } else {
-                cnt++
+                result++
                 map[c] = 1
                 e++
-                result++
+                cnt++
             }
         } else {
             result++
-            map[c] = map[c]!! + 1
+            map[c] = map[c] + 1
             e++
         }
 
