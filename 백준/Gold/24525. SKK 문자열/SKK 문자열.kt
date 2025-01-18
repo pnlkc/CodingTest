f0647535ec8c49bcd7bf47443a13b553c0f025e0
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2025-01-18 15:23:31 +0900

    [Gold II] Title: SKK 문자열, Time: 152 ms, Memory: 17444 KB -BaekjoonHub

---
+++ 백준/Gold/24525. SKK 문자열/SKK 문자열.kt
@@ -0,0 +1,28 @@
+fun main() {
+    val S = readln()
+    val sL = S.length
+    val iL = IntArray(300_001) { -1 }
+    val cnt = Array(2) { IntArray(sL + 1) }
+    var result = -1
+
+    for (i in 0 until sL) {
+        cnt[0][i + 1] = cnt[0][i]
+        cnt[1][i + 1] = cnt[1][i]
+
+        if (S[i] == 'S') cnt[0][i + 1]++
+        if (S[i] == 'K') cnt[1][i + 1]++
+    }
+
+    for (i in 0..sL) {
+        val idx = cnt[0][i] * 2 - cnt[1][i] + 100_000
+
+        if (iL[idx] == -1) {
+            iL[idx] = i
+        } else {
+            if (cnt[0][iL[idx]] == cnt[0][i] && cnt[1][iL[idx]] == cnt[1][i]) continue
+            result = maxOf(result, i - iL[idx])
+        }
+    }
+
+    println(result)
+}
