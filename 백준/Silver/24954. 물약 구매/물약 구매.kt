3c6657692d2e7ad2f91164b28d0d923fd1506f91
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2025-03-26 13:35:31 +0900

    [Silver I] Title: 물약 구매, Time: 3248 ms, Memory: 305152 KB -BaekjoonHub

--- 백준/Silver/24954. 물약 구매/물약 구매.kt
+++ 백준/Silver/24954. 물약 구매/물약 구매.kt
@@ -1,10 +1,9 @@
 var min = Int.MAX_VALUE
-var cL = listOf<Int>()
 var arr = arrayOf<IntArray>()
 
 fun main() {
     val N = readln().toInt()
-    cL = listOf(0) + readln().split(" ").map { it.toInt() }
+    val cL = listOf(0) + readln().split(" ").map { it.toInt() }
     arr = Array(N + 1) { IntArray(N + 1) }
 
 
@@ -18,32 +17,30 @@ fun main() {
         }
     }
 
-    calc(mutableListOf(), N)
+    calc(mutableListOf(), 0, N, cL.toMutableList())
     println(min)
 }
 
-fun calc(pL: MutableList<Int>, n: Int) {
+fun calc(pL: MutableList<Int>, cost:Int, n: Int, cL: MutableList<Int>) {
+    if (min <= cost) return
+
     if (pL.size == n) {
-        val temp = cL.toMutableList()
-        var result = 0
-
-        for (i in 0 until n) {
-            result += temp[pL[i]]
-
-            for (j in 1..n) {
-                temp[j] -= arr[pL[i]][j]
-                if (temp[j] <= 0) temp[j] = 1
-            }
-        }
-
-        min = minOf(min, result)
+        min = minOf(min, cost)
         return
     }
 
     for (i in 1..n) {
         if (pL.contains(i)) continue
-        val temp = pL.toMutableList()
-        temp.add(i)
-        calc(temp, n)
+        val tempPL = pL.toMutableList()
+        val tempCL = cL.toMutableList()
+
+        tempPL.add(i)
+
+        for (j in 1..n) {
+            tempCL[j] -= arr[i][j]
+            if (tempCL[j] <= 0) tempCL[j] = 1
+        }
+
+        calc(tempPL, cost + cL[i], n, tempCL)
     }
 }
