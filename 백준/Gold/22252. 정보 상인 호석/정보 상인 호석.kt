e68d204cdbc3cdb6e5860793cb97cacd343ea19a
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2025-04-27 09:14:40 +0900

    [Gold V] Title: 정보 상인 호석, Time: 1136 ms, Memory: 141132 KB -BaekjoonHub

--- 백준/Gold/22252. 정보 상인 호석/정보 상인 호석.kt
+++ 백준/Gold/22252. 정보 상인 호석/정보 상인 호석.kt
@@ -1,16 +1,19 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import java.util.PriorityQueue
 import java.util.StringTokenizer
 
 fun StringTokenizer.ni() = this.nextToken().toInt()
 
 fun main() {
-    val Q = readln().toInt()
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val Q = br.readLine().toInt()
     var st: StringTokenizer
     val map = mutableMapOf<String, PriorityQueue<Int>>()
     var result = 0L
 
     for (i in 0 until Q) {
-        st = StringTokenizer(readln())
+        st = StringTokenizer(br.readLine())
         val command = st.ni()
         val name = st.nextToken()
 
@@ -23,7 +26,7 @@ fun main() {
         } else {
             for (i in 0 until st.ni()) {
                 if (map[name] == null || map[name]!!.isEmpty()) break
-                
+
                 result += map[name]!!.poll()!!
             }
         }
