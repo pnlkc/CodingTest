ac2c28d4ef51a34e021f95572f859c37da569654
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2025-05-03 10:23:54 +0900

    [Gold V] Title: 빌런 호석, Time: 428 ms, Memory: 89452 KB -BaekjoonHub

--- 백준/Gold/22251. 빌런 호석/빌런 호석.kt
+++ 백준/Gold/22251. 빌런 호석/빌런 호석.kt
@@ -1,51 +1,33 @@
-var arr = intArrayOf()
 val map = intArrayOf(119, 18, 93, 91, 58, 107, 111, 82, 127, 123)
-var result = 0
-var n = 0
-var k = 0
-var p = 0
-var x = 0
 
 fun main() {
     val (N, K, P, X) = readln().split(" ").map { it.toInt() }
-
-    arr = IntArray(K)
-    n = N
-    k = K
-    p = P
-    x = X
-
     var num = X.toString()
+    var result = 0
 
     while (num.length < K) {
         num = "0$num"
     }
 
-    for (i in num.indices) {
-        arr[i] = num[i].digitToInt()
+    p@ for (i in 1..N) {
+        if (i == X) continue
+
+        var cnt = 0
+        var next = i.toString()
+
+        while (next.length < K) {
+            next = "0$next"
+        }
+
+        for (j in num.indices) {
+            cnt += calcDiff(num[j].digitToInt(), next[j].digitToInt())
+            if (cnt > P) continue@p
+        }
+
+        result++
     }
-
-    calc(0, num, 0)
-
+    
     println(result)
 }
 
 fun calcDiff(a: Int, b: Int): Int = (map[a] xor map[b]).countOneBits()
-
-fun calc(idx: Int, num: String, cnt: Int) {
-    if (cnt > p) return
-
-    if (idx >= k) {
-        val cNum = arr.joinToString("").toInt()
-        
-        if (cNum in 1..n && cNum != x) result++
-        return
-    }
-
-    for (i in 0..9) {
-        val nCnt = cnt + calcDiff(num[idx].digitToInt(), i)
-
-        arr[idx] = i
-        if (nCnt <= p) calc(idx + 1, num, nCnt)
-    }
-}
