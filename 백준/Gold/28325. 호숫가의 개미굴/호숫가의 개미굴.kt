291f9bba151e5cce141fc927de50474c127b75a1
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2025-05-11 10:36:38 +0900

    [Gold V] Title: 호숫가의 개미굴, Time: 504 ms, Memory: 76124 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.kt
+++ 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.kt
@@ -1,25 +1,39 @@
 fun main() {
     val N = readln().toInt()
     val cArr = readln().split(" ").map { it.toLong() }
-    val dp = Array(2) { Array(2) { LongArray(N) } }
+    val isPickA = BooleanArray(N)
+    val isPickB = BooleanArray(N)
+    var resultA = 0L
+    var resultB = 0L
 
-    dp[0][0][0] = cArr[0]
-    dp[1][1][0] = 1
+    for (i in 0 until N) {
+        if (cArr[i] != 0L) {
+            resultA += cArr[i]
+            resultB += cArr[i]
+        } else {
+            if (i == N - 1) {
+                if (!isPickA[i - 1] && !isPickA[0]) {
+                    isPickA[N - 1] = true
+                    resultA++
+                }
 
-    for (i in 1 until N) {
-        when (i) {
-            1 -> {
-                dp[0][0][i] = dp[0][0][0] + cArr[i]
-                dp[0][1][i] = dp[0][0][0] + 1
-                dp[1][0][i] = dp[1][1][0] + cArr[i]
-            }
-            else -> {
-                dp[0][0][i] = maxOf(dp[0][0][i - 1], dp[0][1][i - 1]) + cArr[i]
-                dp[1][0][i] = maxOf(dp[1][0][i - 1], dp[1][1][i - 1]) + cArr[i]
-                dp[0][1][i] = dp[0][0][i - 1] + 1
+                if (!isPickB[i - 1] && !isPickB[0]) {
+                    isPickB[N - 1] = true
+                    resultB++
+                }
+            } else {
+                if (i == 0 || !isPickA[i - 1]) {
+                    isPickA[i] = true
+                    resultA++
+                }
+
+                if (i != 0  && !isPickB[i - 1]) {
+                    isPickB[i] = true
+                    resultB++
+                }
             }
         }
     }
-    
-    println(listOf(dp[0][0][N - 1], dp[0][1][N - 1], dp[1][0][N - 1], dp[1][1][N - 1],).max())
+
+    println(maxOf(resultA, resultB))
 }
