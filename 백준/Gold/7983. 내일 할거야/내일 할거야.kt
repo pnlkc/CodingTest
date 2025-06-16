cc4d081a6d7eac39e1af48efcb1d3aadfa10ad4c
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2025-06-16 10:24:26 +0900

    [Gold V] Title: 내일 할거야, Time: 3040 ms, Memory: 360276 KB -BaekjoonHub

---
+++ 백준/Gold/7983. 내일 할거야/내일 할거야.kt
@@ -0,0 +1,24 @@
+data class Data(val d: Int, val t: Int)
+
+fun main() {
+    val n = readln().toInt()
+    val arr = Array(n) { Data(0, 0) }
+
+    for (i in 0 until n) {
+        val (d, t) = readln().split(" ").map { it.toInt() }
+
+        arr[i] = Data(d, t)
+    }
+
+    arr.sortWith(compareBy<Data> { it.t }.thenBy { it.d })
+
+    var result = arr[n - 1].t
+
+    for (i in n - 1 downTo 0) {
+        if (result > arr[i].t) result = arr[i].t
+
+        result -= arr[i].d
+    }
+
+    println(result)
+}
