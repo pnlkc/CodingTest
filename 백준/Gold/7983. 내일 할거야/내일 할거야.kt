af2f3ce34383b8cf76a2362b27c8837b2c500b7a
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2025-06-16 10:33:11 +0900

    [Gold V] Title: 내일 할거야, Time: 2556 ms, Memory: 364992 KB -BaekjoonHub

--- 백준/Gold/7983. 내일 할거야/내일 할거야.kt
+++ 백준/Gold/7983. 내일 할거야/내일 할거야.kt
@@ -1,22 +1,21 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
 data class Data(val d: Int, val t: Int)
 
 fun main() {
-    val n = readln().toInt()
-    val arr = Array(n) { Data(0, 0) }
-
-    for (i in 0 until n) {
-        val (d, t) = readln().split(" ").map { it.toInt() }
-
-        arr[i] = Data(d, t)
-    }
-
-    arr.sortWith(compareBy<Data> { it.t })
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+    val arr = Array(n) {
+        val st = StringTokenizer(br.readLine())
+        Data(st.nextToken().toInt(), st.nextToken().toInt())
+    }.sortedBy { it.t }
 
     var result = arr[n - 1].t
 
     for (i in n - 1 downTo 0) {
         if (result > arr[i].t) result = arr[i].t
-
         result -= arr[i].d
     }
 
