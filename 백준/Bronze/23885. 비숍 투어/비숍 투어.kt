ae30c18150aa8090163bbaa349f9cdb30c9a89dd
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2025-07-04 09:23:22 +0900

    [Bronze I] Title: 비숍 투어, Time: 120 ms, Memory: 20856 KB, Score: 35 point -BaekjoonHub

---
+++ 백준/Bronze/23885. 비숍 투어/비숍 투어.kt
@@ -0,0 +1,27 @@
+fun main() {
+    val (N, M) = readln().split(" ").map { it.toInt() }
+    val (sx, sy) = readln().split(" ").map { it.toInt() }
+    val (ex, ey) = readln().split(" ").map { it.toInt() }
+    var result = false
+
+    when {
+        sx % 2 == 0 && sy % 2 == 0 -> {
+            if (ex % 2 == 0 && ey % 2 == 0) result = true
+            if (ex % 2 != 0 && ey % 2 != 0) result = true
+        }
+        sx % 2 == 0 && sy % 2 != 0 -> {
+            if (ex % 2 == 0 && ey % 2 != 0) result = true
+            if (ex % 2 != 0 && ey % 2 == 0) result = true
+        }
+        sx % 2 != 0 && sy % 2 == 0 -> {
+            if (ex % 2 != 0 && ey % 2 == 0) result = true
+            if (ex % 2 == 0 && ey % 2 != 0) result = true
+        }
+        else -> {
+            if (ex % 2 != 0 && ey % 2 != 0) result = true
+            if (ex % 2 == 0 && ey % 2 == 0) result = true
+        }
+    }
+
+    println(if (result) "YES" else "NO")
+}
