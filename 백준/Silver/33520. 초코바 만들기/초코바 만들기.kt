a28b60ee026162dffeba2a15d20ba4076fddebd3
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2025-09-17 09:17:05 +0900

    [Silver V] Title: 초코바 만들기, Time: 496 ms, Memory: 107416 KB -BaekjoonHub

--- 백준/Silver/33520. 초코바 만들기/초코바 만들기.kt
+++ 백준/Silver/33520. 초코바 만들기/초코바 만들기.kt
@@ -1,10 +1,14 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
 fun main() {
-    val N = readln().toInt()
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val N = br.readLine().toInt()
     var w = 0L
     var h = 0L
 
     for (i in 0 until N) {
-        val (a, b) = readln().split(" ").map { it.toLong() }
+        val (a, b) = br.readLine().split(" ").map { it.toLong() }
 
         w = maxOf(w, minOf(a, b))
         h = maxOf(h, maxOf(a, b))
