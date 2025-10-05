fab53580996c82ab45310420682d8248cf2b06ae
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2025-10-05 10:10:13 +0900

    [Gold IV] Title: 알고리즘 수업 - 행렬 경로 문제 7, Time: 1428 ms, Memory: 339712 KB -BaekjoonHub

--- 백준/Gold/24430. 알고리즘 수업 － 행렬 경로 문제 7/알고리즘 수업 － 행렬 경로 문제 7.kt
+++ 백준/Gold/24430. 알고리즘 수업 － 행렬 경로 문제 7/알고리즘 수업 － 행렬 경로 문제 7.kt
@@ -1,37 +1,48 @@
+data class Data(var sum: Int = 0, var cnt: Int = 0)
+
 fun main() {
     val n = readln().toInt()
     val map = Array(n) { readln().split(" ").map { it.toInt() } }
     val p = readln().toInt()
-    val pL = Array(n) { BooleanArray(n) }
-    val dp = Array(n) { Array(n) { 0 to 0 } }
+    val pL = Array(n) { IntArray(n) }
+    val dp = Array(n) { Array(n) { Data() } }
 
     for (i in 0 until p) {
         val (r, c) = readln().split(" ").map { it.toInt() }
 
-        pL[r - 1][c - 1] = true
+        pL[r - 1][c - 1] = 1
     }
 
-    dp[0][0] = map[0][0] to if (pL[0][0]) 1 else 0
+    dp[0][0] = Data(map[0][0], pL[0][0])
 
     for (i in 0 until n) {
         for (j in 0 until n) {
-            if (i + 1 < n) {
-                if (dp[i + 1][j].first < dp[i][j].first + map[i + 1][j]) {
-                    dp[i + 1][j] = dp[i][j].first + map[i + 1][j] to dp[i][j].second + if (pL[i + 1][j]) 1 else 0
-                } else if (dp[i + 1][j].first == dp[i][j].first + map[i + 1][j]) {
-                    dp[i + 1][j] = dp[i + 1][j].first to maxOf(dp[i + 1][j].second, dp[i][j].second + if (pL[i + 1][j]) 1 else 0)
+            if (i + 1 < n){
+                val nSumA = dp[i][j].sum + map[i + 1][j]
+                val nCntA = dp[i][j].cnt + pL[i + 1][j]
+
+                if (dp[i + 1][j].sum < nSumA) {
+                    dp[i + 1][j].sum = nSumA
+                    dp[i + 1][j].cnt = nCntA
+                } else if (dp[i + 1][j].sum == nSumA) {
+                    dp[i + 1][j].cnt = maxOf(dp[i + 1][j].cnt, nCntA)
                 }
+
             }
 
             if (j + 1 < n) {
-                if (dp[i][j + 1].first < dp[i][j].first + map[i][j + 1]) {
-                    dp[i][j + 1] = dp[i][j].first + map[i][j + 1] to dp[i][j].second + if (pL[i][j + 1]) 1 else 0
-                } else if (dp[i][j + 1].first == dp[i][j].first + map[i][j + 1]) {
-                    dp[i][j + 1] = dp[i][j + 1].first to maxOf(dp[i][j + 1].second, dp[i][j].second + if (pL[i][j + 1]) 1 else 0)
+                val nSumB = dp[i][j].sum + map[i][j + 1]
+                val nCntB = dp[i][j].cnt + pL[i][j + 1]
+
+                if (dp[i][j + 1].sum < nSumB) {
+                    dp[i][j + 1].sum = nSumB
+                    dp[i][j + 1].cnt = nCntB
+                } else if (dp[i][j + 1].sum == nSumB) {
+                    dp[i][j + 1].cnt = maxOf(dp[i][j + 1].cnt, nCntB)
                 }
             }
         }
     }
 
-    println("${dp[n - 1][n - 1].first} ${dp[n - 1][n - 1].second}")
+    println("${dp[n - 1][n - 1].sum} ${dp[n - 1][n - 1].cnt}")
 }
