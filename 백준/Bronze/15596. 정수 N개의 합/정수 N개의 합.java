1b8fdffefbffcca36808ab035507d78c03af1354
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2026-01-25 09:32:00 +0900

    [Bronze II] Title: 정수 N개의 합, Time: 36 ms, Memory: 423036 KB -BaekjoonHub

---
+++ 백준/Bronze/15596. 정수 N개의 합/정수 N개의 합.java
@@ -0,0 +1,11 @@
+public class Test {
+    long sum(int[] a) {
+        long result = 0L;
+
+        for (int i = 0; i < a.length; i++) {
+            result += a[i];
+        }
+
+        return result;
+    }
+}
