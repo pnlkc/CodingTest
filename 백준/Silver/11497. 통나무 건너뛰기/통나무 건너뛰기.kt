87d609f5dd7902b47f984e240ec78f4dfd95d7a8
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2026-01-26 09:30:11 +0900

    [Silver I] Title: 통나무 건너뛰기, Time: 648 ms, Memory: 68224 KB -BaekjoonHub

--- 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.kt
+++ 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.kt
@@ -4,11 +4,12 @@ fun main() {
 
     for (tc in 1..t) {
         val n = readln().toInt()
-        val list = readln().split(" ").map { it.toInt() }.sorted()
+        val list = readln().split(" ").map { it.toInt() }.toIntArray().sortedArray()
         var max = maxOf(list[1] - list[0], list[n - 1] - list[n - 2])
 
-        for (i in 0 until n - 2) {
+        for (i in 0 until n - 2 step 2) {
             max = maxOf(max, list[i + 2] - list[i])
+            if (i + 3 < n) max = maxOf(max, list[i + 3] - list[i + 1])
         }
 
         sb.appendLine(max)
