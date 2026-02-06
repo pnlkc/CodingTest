fc780b75d9ada3accd2188f82412bca05ca5446b
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2026-02-06 09:52:12 +0900

    [Silver III] Title: 부정행위 멈춰!, Time: 396 ms, Memory: 92464 KB -BaekjoonHub

--- 백준/Silver/28356. 부정행위 멈춰！/부정행위 멈춰！.kt
+++ 백준/Silver/28356. 부정행위 멈춰！/부정행위 멈춰！.kt
@@ -3,29 +3,13 @@ fun main() {
     val (n, m) = readln().split(" ").map { it.toInt() }
     val arr = Array(n) { IntArray(m) }
     var result = 1
-    val dx = intArrayOf(1, 1, 1, -1, -1, -1, 0, 0)
-    val dy = intArrayOf(1, 0, -1, 1, 0, -1, 1, -1)
 
     for (i in 0 until n) {
+        val num = if (i % 2 == 0) 1 else if (m == 1) 2 else 3
+
         for (j in 0 until m) {
-            val temp = BooleanArray(10)
-
-            for (d in 0.. 7) {
-                val nx = j + dx[d]
-                val ny = i + dy[d]
-
-                if (nx < 0 || ny < 0 || ny >= n || nx >= m) continue
-
-                temp[arr[ny][nx]] = true
-            }
-
-            for (k in 1..9) {
-                if (!temp[k]) {
-                    arr[i][j] = k
-                    result = maxOf(result, k)
-                    break
-                }
-            }
+            arr[i][j] = num + j % 2
+            result = maxOf(result, arr[i][j])
         }
     }
 
