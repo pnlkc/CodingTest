568165893937a8865299b9fa07c41a3d293d605c
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2026-02-15 09:05:58 +0900

    [Bronze V] Title: 이중 반복문, Time: 64 ms, Memory: 12348 KB -BaekjoonHub

--- 백준/Bronze/34849. 이중 반복문/이중 반복문.kt
+++ 백준/Bronze/34849. 이중 반복문/이중 반복문.kt
@@ -1,5 +1,8 @@
+import kotlin.math.sqrt
+
 fun main() {
     val n = readln().toInt()
+    val max = sqrt(100_000_000.toDouble()).toInt()
 
-    println(if (n.toLong() * n > 100_000_000) "Time limit exceeded" else "Accepted")
+    println(if (max < n) "Time limit exceeded" else "Accepted")
 }
