f9a9567d5872229b55f3296368ea541221bf11d6
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2026-03-01 09:24:56 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 80 ms, Memory: 12312 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val n = readln().toInt()
+    val dp = LongArray(n + 1)
+
+    dp[1] = 1
+
+    for (i in 2..n) {
+        dp[i] = dp[i - 1] + dp[i - 2]
+    }
+
+    println(dp[n])
+}
