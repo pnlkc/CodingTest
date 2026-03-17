bd112c3fbecad9104b66e0a21f08b67cf0957aa4
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2026-03-17 09:26:44 +0900

    [Silver III] Title: 카드 문자열, Time: 316 ms, Memory: 33348 KB -BaekjoonHub

---
+++ 백준/Silver/13417. 카드 문자열/카드 문자열.kt
@@ -0,0 +1,34 @@
+import java.util.LinkedList
+
+fun main() {
+    val sb = StringBuilder()
+    val t = readln().toInt()
+
+    for (tc in 1..t) {
+        val n = readln().toInt()
+        val list = readln().split(" ")
+        val dq = LinkedList<Char>()
+
+        for (i in 0 until n) {
+            val c = list[i][0]
+
+            if (dq.isEmpty()) {
+                dq.add(c)
+            } else {
+                if (dq.peekFirst()!! >= c) {
+                    dq.addFirst(c)
+                } else {
+                    dq.addLast(c)
+                }
+            }
+        }
+
+        while (dq.isNotEmpty()) {
+            sb.append(dq.pollFirst()!!)
+        }
+
+        sb.appendLine()
+    }
+
+    print(sb)
+}
