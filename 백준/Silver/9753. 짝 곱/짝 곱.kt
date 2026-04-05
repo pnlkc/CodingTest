6ea7b221b0a854c4098ede4ae1ae74f04ff7bc02
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2026-04-05 10:17:16 +0900

    [Silver II] Title: 짝 곱, Time: 212 ms, Memory: 18128 KB -BaekjoonHub

--- 백준/Silver/9753. 짝 곱/짝 곱.kt
+++ 백준/Silver/9753. 짝 곱/짝 곱.kt
@@ -2,6 +2,7 @@ fun main() {
     val MAX = 100_000
     val arr = BooleanArray(MAX + 1) { true }
     val list = mutableListOf<Int>()
+    val possible = mutableListOf<Long>()
 
     for (i in 2 until MAX) {
         var num = i + i
@@ -15,19 +16,21 @@ fun main() {
     for (i in 2..MAX) {
         if (arr[i]) list.add(i)
     }
+    
+    p@ for (i in list.indices) {
+        for (j in i + 1..list.lastIndex) {
+            val num = list[i].toLong() * list[j]
+
+            possible.add(num)
+            if (num >= MAX) continue@p
+        }
+    }
+
+    possible.sort()
 
     repeat(readln().toInt()) {
         val k = readln().toInt()
-        var min = Long.MAX_VALUE
 
-        for (i in list.indices) {
-            for (j in i + 1..list.lastIndex) {
-                val num = list[i].toLong() * list[j]
-
-                if (num >= k) min = minOf(min, num)
-            }
-        }
-
-        println(min)
+        println(possible.find { it >= k })
     }
 }
