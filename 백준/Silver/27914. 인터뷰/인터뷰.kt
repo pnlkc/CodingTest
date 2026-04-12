166626f1843f0f83b0322521434af1a6aa3ea9d4
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2026-04-12 09:25:35 +0900

    [Silver II] Title: 인터뷰, Time: 340 ms, Memory: 44068 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/27914. 인터뷰/인터뷰.kt
+++ 백준/Silver/27914. 인터뷰/인터뷰.kt
@@ -1,23 +1,30 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
 fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
     val sb = StringBuilder()
-    val (n, k, q) = readln().split(" ").map { it.toInt() }
-    val aL = readln().split(" ").map { it.toInt() }
-    val xL = readln().split(" ").map { it.toInt() }
+    var st = StringTokenizer(br.readLine())
+    val n = st.nextToken().toInt()
+    val k = st.nextToken().toInt()
+    val q = st.nextToken().toInt()
     val dp = IntArray(n + 1)
     val preSum = LongArray(n + 1)
 
+    st = StringTokenizer(br.readLine())
+
     for (i in 0 until n) {
-        if (aL[i] == k) {
-            dp[i + 1] = 0
-            preSum[i + 1] = preSum[i]
-        } else {
-            dp[i + 1] = dp[i] + 1
-            preSum[i + 1] = preSum[i] + dp[i + 1]
-        }
+        val c = st.nextToken().toInt()
+
+        dp[i + 1] = if (c == k) 0 else dp[i] + 1
+        preSum[i + 1] = preSum[i] + dp[i + 1]
     }
 
-    for (i in 0 until q) {
-        sb.appendLine(preSum[xL[i]])
+    st = StringTokenizer(br.readLine())
+
+    repeat(q) {
+        sb.appendLine(preSum[st.nextToken().toInt()])
     }
 
     print(sb)
