d1d83ef894490364abdb3ead8d5a52b56431abe2
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2026-04-22 09:22:44 +0900

    [Bronze V] Title: 시험 성적, Time: 92 ms, Memory: 12392 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var input = readLine()!!.toInt()
+    when (input) {
+        in 90..100 -> println("A")
+        in 80..89 -> println("B")
+        in 70..79 -> println("C")
+        in 60..69 -> println("D")
+        else -> println("F")
+    }
+}
