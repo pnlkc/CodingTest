315a1b4594a097bcddb06b286ee38c1ffc0fc9b0
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2026-06-08 08:29:58 +0900

    Create 숫자 더하기.kt

---
+++ goormlevel/353439/1. 숫자 더하기/숫자 더하기.kt
@@ -0,0 +1,9 @@
+// https://level.goorm.io/exam/353439/%EC%88%AB%EC%9E%90-%EB%8D%94%ED%95%98%EA%B8%B0/quiz/1
+
+fun main() {
+    val (a, b, c, d) = readLine()!!.split(" ")
+    val f = (a + b).toLong()
+    val s = (c + d).toLong()
+
+    println(f + s)
+}
