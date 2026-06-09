ea6e5d71f254f1de9679bc00b1209456a14fa9a4
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2026-06-10 08:30:05 +0900

    Create 포도 와인.kt

---
+++ goormlevel/354507/1. 포도 와인/포도 와인.kt
@@ -0,0 +1,7 @@
+// https://level.goorm.io/exam/354507/%ED%8F%AC%EB%8F%84-%EC%99%80%EC%9D%B8/quiz/1
+
+fun main() {
+    val (a, b, c) = readLine()!!.split(" ").map { it.toInt() }
+
+    println(a * b / c)
+}
