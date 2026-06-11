1a71dd88ab54cbef500f2510b8a25e6098f2bd3c
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2026-06-12 08:33:11 +0900

    Create 앞뒤 문자 찾기.kt

---
+++ goormlevel/353236/1. 앞뒤 문자 찾기/앞뒤 문자 찾기.kt
@@ -0,0 +1,7 @@
+// https://level.goorm.io/exam/353236/%EC%95%9E%EB%92%A4-%EB%AC%B8%EC%9E%90-%EC%B0%BE%EA%B8%B0/quiz/1
+
+fun main() {
+    val s = readLine()!!
+
+    println("${s.first()}${s.last()}")
+}
