b3dca2aa7142d765380f853a8322438cf211fa29
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2026-09-11 08:34:46 +0900

    [D1] Title: [파이썬 프로그래밍 기초(2) 파이썬의 기본 응용] 5. 객체지향 7, Time: 59 ms, Memory: 53,248 KB -BaekjoonHub

--- SWEA/D1/6229. ［파이썬 프로그래밍 기초（2） 파이썬의 기본 응용］ 5． 객체지향 7/［파이썬 프로그래밍 기초（2） 파이썬의 기본 응용］ 5． 객체지향 7.py
+++ SWEA/D1/6229. ［파이썬 프로그래밍 기초（2） 파이썬의 기본 응용］ 5． 객체지향 7/［파이썬 프로그래밍 기초（2） 파이썬의 기본 응용］ 5． 객체지향 7.py
@@ -7,15 +7,15 @@ class Person:
 
 
 class Male(Person):
-    def getGender():
+    def getGender(self):
         return 'Male'
 
 class Female(Person):
-    def getGender():
+    def getGender(self):
         return 'Female'
 
-male = Male
-female = Female
+male = Male()
+female = Female()
 
 print(male.getGender())
 print(female.getGender())
