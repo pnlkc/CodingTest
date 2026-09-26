f063b306ad978333c00880a691131a6a81d72c80
Jichan Choi <102578749+pnlkc@users.noreply.github.com>
2026-09-26 09:10:37 +0900

    [D1] Title: [파이썬 프로그래밍 기초(2) 파이썬의 기본 응용] 5. 객체지향 2, Time: 57 ms, Memory: 52,736 KB -BaekjoonHub

---
+++ SWEA/D1/6208. ［파이썬 프로그래밍 기초（2） 파이썬의 기본 응용］ 5． 객체지향 2/［파이썬 프로그래밍 기초（2） 파이썬의 기본 응용］ 5． 객체지향 2.py
@@ -0,0 +1,12 @@
+class Korean:
+    def __init__(self):
+        pass
+
+    def printNationality(self):
+        print('대한민국')
+
+korean1 = Korean()
+korean2 = Korean()
+
+korean1.printNationality()
+korean2.printNationality()
